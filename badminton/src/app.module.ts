a95e084f57c6940d12eb528a40f21f94422ac6ab
sjk5766 <wjdrnr6662@gmail.com>
2024-11-25 21:12:04 +0900

    feat: app module에 typeorm 설정 반영

--- badminton/src/app.module.ts
+++ badminton/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from './../ormconfig';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(connectionOptions)],
 })
 export class AppModule {}
