ab6bcf667ae7a2989e0cc5d6bfaba2d6d13b137d
sjk5766 <wjdrnr6662@gmail.com>
2024-11-26 14:32:19 +0900

    feat: app 모듈에 coach 모듈 추가

--- badminton/src/app.module.ts
+++ badminton/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from './../ormconfig';
+import { CoachModule } from './coach/coach.module';
 import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), LessonModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions),
+    CoachModule,
+    LessonModule,
+  ],
 })
 export class AppModule {}
