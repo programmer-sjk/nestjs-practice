c9f087de07ac5767e1913e4ba447aba85e6c2e4f
sjk5766 <wjdrnr6662@gmail.com>
2024-11-26 15:22:25 +0900

    chore: lesson 마이그레이션 파일 추가

---
+++ badminton/src/migrations/1732602044742-lesson.ts
@@ -0,0 +1,24 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Lesson1732602044742 implements MigrationInterface {
+  name = 'Lesson1732602044742';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`lesson\` (
+        \`id\` int NOT NULL AUTO_INCREMENT, 
+        \`type\` enum ('ONE_TIME', 'REGULAR') NOT NULL, 
+        \`lessonMinute\` int NOT NULL, 
+        \`customerName\` varchar(18) NOT NULL, 
+        \`customerPhone\` varchar(11) NOT NULL, 
+        \`password\` varchar(255) NOT NULL, 
+        \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+        \`deletedAt\` datetime(6) NULL, \`coachId\` int NULL, 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`lesson\``);
+  }
+}
