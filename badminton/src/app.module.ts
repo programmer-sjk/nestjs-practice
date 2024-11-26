ab020b5140fa073ac5e693ba53ddb78809f95c68
sjk5766 <wjdrnr6662@gmail.com>
2024-11-26 14:06:47 +0900

    feat: app module에 lesson 모듈 추가

--- badminton/src/app.module.ts
+++ badminton/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from './../ormconfig';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions)],
+  imports: [TypeOrmModule.forRoot(connectionOptions), LessonModule],
 })
 export class AppModule {}
