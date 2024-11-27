9ff42ac3cb34e0c9b73bbbaa21ebc274c599f435
sjk5766 <wjdrnr6662@gmail.com>
2024-11-27 13:05:20 +0900

    feat: response entity 추가

---
+++ badminton/src/common/response-entity.ts
@@ -0,0 +1,22 @@
+export class ReponseEntity<T> {
+  private static readonly SUCCESS = true;
+  private static readonly FAIL = true;
+
+  readonly success: boolean;
+  readonly message: string;
+  readonly data: T;
+
+  private constructor(success: boolean, message: string, data: T) {
+    this.success = success;
+    this.message = message;
+    this.data = data;
+  }
+
+  static OK<T>(data?: T) {
+    return new ReponseEntity(ReponseEntity.SUCCESS, '', data ?? '');
+  }
+
+  static ERROR(message: string) {
+    return new ReponseEntity(ReponseEntity.FAIL, message, '');
+  }
+}
