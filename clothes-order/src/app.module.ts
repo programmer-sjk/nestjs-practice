1139861c37e5e641ec6729b3e93149e023d86379
sjk5766 <wjdrnr6662@gmail.com>
2024-11-29 16:14:43 +0900

    feat: 각 모듈 추가

--- clothes-order/src/app.module.ts
+++ clothes-order/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomerModule } from './customer/customer.module';
+import { OrderModule } from './order/order.module';
+import { StoreModule } from './store/store.module';
+import { BillingModule } from './billing/billing.module';
 
 @Module({
-  imports: [],
+  imports: [CustomerModule, OrderModule, StoreModule, BillingModule],
   controllers: [AppController],
   providers: [AppService],
 })
