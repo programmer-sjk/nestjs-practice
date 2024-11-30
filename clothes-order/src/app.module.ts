7257b1baca7364b85fb59aa9466a50e277420f88
sjk5766 <wjdrnr6662@gmail.com>
2024-11-30 15:40:18 +0900

    feat: app 모듈에 typeorm 설정 추가

--- clothes-order/src/app.module.ts
+++ clothes-order/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../ormconfig';
+import { BillingModule } from './billing/billing.module';
 import { CustomerModule } from './customer/customer.module';
 import { OrderModule } from './order/order.module';
 import { StoreModule } from './store/store.module';
-import { BillingModule } from './billing/billing.module';
 
 @Module({
-  imports: [CustomerModule, OrderModule, StoreModule, BillingModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions),
+    CustomerModule,
+    OrderModule,
+    StoreModule,
+    BillingModule,
+  ],
 })
 export class AppModule {}
