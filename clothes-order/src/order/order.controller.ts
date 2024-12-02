5385ce5514764b944c5a48863092f00e0a601e75
sjk5766 <wjdrnr6662@gmail.com>
2024-12-02 18:17:20 +0900

    feat: 기존 응답에 response entity 반영

--- clothes-order/src/order/order.controller.ts
+++ clothes-order/src/order/order.controller.ts
@@ -1,5 +1,8 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ResponseEntity } from '../common/response-entity';
 import { AddClothesOrderRequest } from './dto/add-clothes-order-request';
+import { OrderResponse } from './dto/order-response';
+import { ReturnOrderRequest } from './dto/return-order-request';
 import { OrderService } from './order.service';
 
 @Controller('orders')
@@ -7,17 +10,31 @@ export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
   @Get()
-  async orders() {
+  async orders(): Promise<ResponseEntity<OrderResponse[] | string>> {
     const userId = 1;
-    return this.orderService.findOrders(userId);
+    try {
+      const orders = await this.orderService.findOrders(userId);
+      return ResponseEntity.OK(orders);
+    } catch (e) {
+      return ResponseEntity.ERROR(e.message);
+    }
   }
 
   @Post()
-  async addOrder(@Body() request: AddClothesOrderRequest) {
+  async addOrder(
+    @Body() request: AddClothesOrderRequest,
+  ): Promise<ResponseEntity<string>> {
     const userId = 1;
     await this.orderService.addOrder(userId, request);
+    return ResponseEntity.OK();
   }
 
-  // @Post('/return')
-  // async returnOrder(@Body() request: ReturnOrderRequest) {}
+  @Post('/return')
+  async returnOrder(
+    @Body() request: ReturnOrderRequest,
+  ): Promise<ResponseEntity<string>> {
+    const userId = 1;
+    await this.orderService.returnClothes(userId, request);
+    return ResponseEntity.OK();
+  }
 }
