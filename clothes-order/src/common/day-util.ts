c16aa86661693802d47fd49b7b733fd219303674
sjk5766 <wjdrnr6662@gmail.com>
2024-12-02 18:50:54 +0900

    feat: DayUtil 클래스에 toDate 메서드 추가

--- clothes-order/src/common/day-util.ts
+++ clothes-order/src/common/day-util.ts
@@ -3,6 +3,10 @@ import * as dayjs from 'dayjs';
 export class DayUtil {
   private constructor() {}
 
+  static toDate(target?: Date) {
+    return dayjs(target).toDate();
+  }
+
   static diff(target: Date) {
     return dayjs().diff(target, 'day');
   }
