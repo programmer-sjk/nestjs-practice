c6f103b9afb8797d4164b1916977ea45f8698075
sjk5766 <wjdrnr6662@gmail.com>
2024-12-02 20:52:09 +0900

    feat: main 파일에 swagger 설정 적용

--- clothes-order/src/main.ts
+++ clothes-order/src/main.ts
@@ -1,11 +1,30 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('api/v1');
+
+  bootstrapSwagger(app);
   await app.listen(3000);
 }
+
+function bootstrapSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('API example')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+}
+
 bootstrap();
