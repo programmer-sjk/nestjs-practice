48215ba758c4835a9c57753323844ec7e29ae77c
sjk5766 <wjdrnr6662@gmail.com>
2024-12-02 18:52:00 +0900

    feat: repository 디렉토리 추가 & order item repository 추가

---
+++ clothes-order/src/order/repositories/order.repository.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { Order } from '../entities/order.entity';
+import { OrderItemStatus } from '../enum/order-item-status.enum';
+
+@Injectable()
+export class OrderRepository extends Repository<Order> {
+  constructor(private readonly dataSource: DataSource) {
+    super(Order, dataSource.createEntityManager());
+  }
+
+  findWithItems(customerId: number, orderItemIds: number[]) {
+    return this.createQueryBuilder('order')
+      .innerJoinAndSelect('order.orderItems', 'orderItems')
+      .where('order.customerId := customerId', { customerId })
+      .andWhere('orderItems.id IN (:orderItemIds)', { orderItemIds })
+      .andWhere('orderItems.status = :status', {
+        status: OrderItemStatus.STORED,
+      })
+      .getMany();
+  }
+}
