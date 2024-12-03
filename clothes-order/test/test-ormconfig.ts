58daa93ad15c1aa1b1315fe19d23795e4046547b
sjk5766 <wjdrnr6662@gmail.com>
2024-12-03 16:41:13 +0900

    chore: test용 ormconfig 추가

---
+++ clothes-order/test/test-ormconfig.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const testConnectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 5432,
+  username: 'root',
+  password: 'password',
+  database: 'test',
+  logging: false,
+  synchronize: true,
+  entities: [__dirname + '../../src/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '../../src/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+};
+
+const dataSource = new DataSource(testConnectionOptions);
+export default dataSource;
