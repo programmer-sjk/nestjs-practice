13796ef256fa1fe2054fef942b9db8bcd94d0dae
sjk5766 <wjdrnr6662@gmail.com>
2024-12-04 13:38:14 +0900

    test: customer, order, order-item fixture class 추가

---
+++ clothes-order/test/fixture/test-item-order.creator.ts
@@ -0,0 +1,18 @@
+import { OrderItemStatus } from '../../src/order/enum/order-item-status.enum';
+import { OrderItemType } from '../../src/order/enum/order-item-type.enum';
+import { OrderItem } from './../../src/order/entities/order-item.entity';
+
+export class TestOrderItemCreator {
+  private constructor() {}
+
+  static of() {
+    const item = new OrderItem();
+    item.status = OrderItemStatus.STORED;
+    item.type = OrderItemType.CLOTHES;
+    item.category = 'TOP';
+    item.subCategory = 'JEAN';
+    item.count = 1;
+
+    return item;
+  }
+}
---
+++ clothes-order/test/fixture/test-order.creator.ts
@@ -0,0 +1,25 @@
+import { Order } from '../../src/order/entities/order.entity';
+import { Price } from '../../src/order/entities/price';
+import { DeliveryStatus } from '../../src/order/enum/delivery-status.enum';
+import { OrderStatus } from '../../src/order/enum/order-status.enum';
+import { Customer } from './../../src/customer/entities/customer.entity';
+
+export class TestOrderCreator {
+  private constructor() {}
+
+  static of(customer?: Customer) {
+    const order = new Order();
+    order.price = Price.of(1_000).get();
+    order.status = OrderStatus.IN_PROGRESS;
+    order.deliveryStatus = DeliveryStatus.STORED;
+
+    if (customer) {
+      order.customerId = customer.id;
+      order.customerZipCode = customer.zipCode;
+      order.customerAddress = customer.address;
+      order.customerAddressDetail = customer.addressDetail;
+    }
+
+    return order;
+  }
+}
