3b003028999b34fb26b07abe0e273a2df6e9dcb0
sjk5766 <wjdrnr6662@gmail.com>
2024-12-10 12:58:20 +0900

    chore: test ormconfig 추가

---
+++ badminton/test/test-ormconfig.ts
@@ -0,0 +1,15 @@
+import { DataSourceOptions } from 'typeorm';
+
+export const testConnectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'test',
+  logging: false,
+  synchronize: true,
+  entities: [__dirname + '/../src/**/*.entity{.ts,.js}'],
+  migrations: [__dirname + '/../src/migrations/*{.ts,.js}'],
+  migrationsTableName: 'migrations',
+};
