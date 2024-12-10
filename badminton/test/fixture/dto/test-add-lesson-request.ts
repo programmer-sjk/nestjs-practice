98636d2ad9cb2ce8ab79109c3cdca0bf2a481528
sjk5766 <wjdrnr6662@gmail.com>
2024-12-10 13:24:53 +0900

    test: dto fixture 추가

---
+++ badminton/test/fixture/dto/test-add-lesson-request.ts
@@ -0,0 +1,18 @@
+import { LessonType } from '../../../src/lesson/enums/lesson-type.enum';
+import { AddLessonRequest } from './../../../src/lesson/dto/add-lesson-request';
+import { TestLessonTimeDto } from './test-lesson-time-dto';
+
+export class TestAddLessonRequest {
+  private constructor() {}
+
+  static oneTime(coachId: number, customerName: string, customerPhone: string) {
+    const dto = new AddLessonRequest();
+    dto.coachId = coachId;
+    dto.type = LessonType.ONE_TIME;
+    dto.customerName = customerName;
+    dto.customerPhone = customerPhone;
+    dto.lessonTimes = [TestLessonTimeDto.oneTime()];
+
+    return dto;
+  }
+}
