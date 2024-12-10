3c89c0dd72d77577e4d0b6bb38fd272d51957a0d
sjk5766 <wjdrnr6662@gmail.com>
2024-12-10 13:23:37 +0900

    feat: lesson time 레포지터리 추가

---
+++ badminton/src/lesson/lesson-time.repository.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { DataSource, Repository } from 'typeorm';
+import { LessonTime } from './entities/lesson-time.entity';
+
+@Injectable()
+export class LessonTimeRepository extends Repository<LessonTime> {
+  constructor(private readonly dataSource: DataSource) {
+    super(LessonTime, dataSource.createEntityManager());
+  }
+}
--- badminton/src/lesson/lesson.module.ts
+++ badminton/src/lesson/lesson.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Lesson } from './entities/lesson.entity';
-import { LessonRepository } from './lesson.repository';
+import { LessonTimeRepository } from './lesson-time.repository';
 import { LessonController } from './lesson.controller';
+import { LessonRepository } from './lesson.repository';
 import { LessonService } from './lesson.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Lesson])],
   controllers: [LessonController],
-  providers: [LessonService, LessonRepository],
+  providers: [LessonService, LessonRepository, LessonTimeRepository],
 })
 export class LessonModule {}
