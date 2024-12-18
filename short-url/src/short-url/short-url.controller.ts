b13cd4d33adc8756a1e5e4f159a72af5c4f804b0
sjk5766 <wjdrnr6662@gmail.com>
2024-12-18 13:28:43 +0900

    feat: type에 따라 short url을 생성하는 구조 추가

--- short-url/src/short-url/short-url.controller.ts
+++ short-url/src/short-url/short-url.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Param, Post } from '@nestjs/common';
+import { BadRequestException, Controller, Param, Post } from '@nestjs/common';
 import { CreateType } from './enums/create-type.enum';
+import { ShortUrlService } from './short-url.service';
 
 @Controller('short-url')
 export class ShortUrlController {
+  constructor(private readonly shortUrlService: ShortUrlService) {}
+
   @Post(':type')
   async addShortUrl(@Param('type') type: CreateType) {
     switch (type) {
-      case CreateType.HASH: return
+      case CreateType.HASH:
+        return this.shortUrlService.shortUrlByHash();
+      case CreateType.RAW:
+        return this.shortUrlService.shortUrlByBaseCalculation();
+      case CreateType.LIB:
+        return this.shortUrlService.shortUrlByBase62();
+      default:
+        throw new BadRequestException('허용하지 않는 생성 타입입니다.');
     }
   }
 }
--- short-url/src/short-url/short-url.service.ts
+++ short-url/src/short-url/short-url.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ShortUrlService {}
+export class ShortUrlService {
+  async shortUrlByHash() {}
+
+  async shortUrlByBaseCalculation() {}
+
+  async shortUrlByBase62() {}
+}
