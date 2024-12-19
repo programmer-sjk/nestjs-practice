1cd4bafd410e20a9421b93de24a8697114ca4ebb
sjk5766 <wjdrnr6662@gmail.com>
2024-12-19 13:11:57 +0900

    chore: ormconfig 파일 추가

---
+++ short-url/ormconfig.ts
@@ -0,0 +1,18 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const connectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'short',
+  logging: true,
+  synchronize: false,
+  entities: [__dirname + '/src/**/entities/*.{js,ts}'],
+  migrations: [__dirname + '/src/migrations/*.{js,ts}'],
+  migrationsTableName: 'migrations',
+};
+
+const dataSource = new DataSource(connectionOptions);
+export default dataSource;
