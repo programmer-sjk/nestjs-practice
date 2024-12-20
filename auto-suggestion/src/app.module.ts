1ffff48832faec9054336c6ecd41d95bddadc2c7
sjk5766 <wjdrnr6662@gmail.com>
2024-12-20 13:40:31 +0900

    feat: app 모듈에 typeorm root 설정 추가

--- auto-suggestion/src/app.module.ts
+++ auto-suggestion/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../ormconfig';
 import { SuggestionModule } from './suggestion/suggestion.module';
 
 @Module({
-  imports: [SuggestionModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), SuggestionModule],
 })
 export class AppModule {}
