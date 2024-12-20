fcd3a63470d98bad6e7a3b11d1a26ae3b6818b06
sjk5766 <wjdrnr6662@gmail.com>
2024-12-20 13:50:29 +0900

    chore: 마이그레이션 파일 추가

---
+++ auto-suggestion/src/migrations/1734669995763-search-daily-sum.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class SearchDailySum1734669995763 implements MigrationInterface {
+  name = 'SearchDailySum1734669995763';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`search_daily_sum\` (
+        \`id\` int NOT NULL AUTO_INCREMENT, 
+        \`keyword\` varchar(255) NOT NULL, 
+        \`count\` int NOT NULL, 
+        \`date\` datetime NOT NULL, 
+        \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`search_daily_sum\``);
+  }
+}
