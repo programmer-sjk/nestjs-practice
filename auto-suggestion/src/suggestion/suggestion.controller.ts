efbfb4611eb7fb3b44f313014302f0c44fb1da40
sjk5766 <wjdrnr6662@gmail.com>
2024-12-21 16:30:17 +0900

    feat: 자동완성 키워드 조회기능

--- auto-suggestion/src/suggestion/suggestion.controller.ts
+++ auto-suggestion/src/suggestion/suggestion.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
+import { AutoCompleteKeywords } from './dto/auto-complete-keywords.request';
 import { SuggestionService } from './suggestion.service';
 
 @Controller('suggestion')
 export class SuggestionController {
   constructor(private readonly suggestionService: SuggestionService) {}
+
+  @Get()
+  async autoCompleteKeywords(@Query() request: AutoCompleteKeywords) {
+    return this.suggestionService.getAutoSuggestions(request);
+  }
 
   @Post()
   async createAutoSuggestions() {
--- auto-suggestion/src/suggestion/suggestion.service.ts
+++ auto-suggestion/src/suggestion/suggestion.service.ts
@@ -1,20 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import * as createTrie from 'autosuggest-trie';
+import { AutoCompleteKeywords } from './dto/auto-complete-keywords.request';
 import { SearchDailySum } from './entities/search-daily-sum.entity';
 import { SearchDailySumRepository } from './suggestion.repository';
 
 @Injectable()
 export class SuggestionService {
+  private readonly SUGGESTION_COUNT = 5;
   private trie;
 
   constructor(
     private readonly searchDailySumRepository: SearchDailySumRepository,
   ) {}
 
+  async getAutoSuggestions(request: AutoCompleteKeywords) {
+    return this.trie
+      .getMatches(request.keyword)
+      .slice(0, this.SUGGESTION_COUNT)
+      .map((suggestion) => suggestion.name);
+  }
+
   async createAutoSuggestions() {
     const dailySum = await this.searchDailySumRepository.find();
     const searchPopulation = this.calculatePopulaiton(dailySum);
-    this.trie = createTrie(searchPopulation, 'name');
+    this.trie = createTrie(searchPopulation, 'name', {
+      comparator: (item1, item2) =>
+        item1.populaiton > item2.populaiton ? -1 : 1,
+    });
   }
 
   private calculatePopulaiton(dailySum: SearchDailySum[]) {
