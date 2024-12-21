bdfd4512969485fbea2db66a343291f37e9d3a4e
sjk5766 <wjdrnr6662@gmail.com>
2024-12-21 16:03:53 +0900

    feat: 자동완성어 생성하는 기능

--- auto-suggestion/src/suggestion/suggestion.service.ts
+++ auto-suggestion/src/suggestion/suggestion.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import * as createTrie from 'autosuggest-trie';
 import { SearchDailySum } from './entities/search-daily-sum.entity';
 import { SearchDailySumRepository } from './suggestion.repository';
 
 @Injectable()
 export class SuggestionService {
+  private trie;
+
   constructor(
     private readonly searchDailySumRepository: SearchDailySumRepository,
   ) {}
@@ -11,16 +14,17 @@ export class SuggestionService {
   async createAutoSuggestions() {
     const dailySum = await this.searchDailySumRepository.find();
     const searchPopulation = this.calculatePopulaiton(dailySum);
+    this.trie = createTrie(searchPopulation, 'name');
   }
 
   private calculatePopulaiton(dailySum: SearchDailySum[]) {
     const populaitonMap = {};
     for (const sum of dailySum) {
-      const target = populaitonMap[sum.keyword];
-      if (target) {
-        target.population += sum.count;
+      if (populaitonMap[sum.keyword]) {
+        populaitonMap[sum.keyword] += sum.count;
       } else {
         populaitonMap[sum.keyword] = sum.count;
+        console.log(populaitonMap);
       }
     }
 
