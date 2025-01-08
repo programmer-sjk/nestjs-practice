3d5246089a5a6d1af36f9df4d0ecd8d507183c4e
sjk5766 <wjdrnr6662@gmail.com>
2025-01-08 13:15:46 +0900

    feat: AddUserRequest에 toEntity 메서드 추가

--- aop/src/user/dto/add-user-request.ts
+++ aop/src/user/dto/add-user-request.ts
@@ -1,7 +1,12 @@
 import { IsNotEmpty, IsString } from 'class-validator';
+import { User } from '../entities/user.entity';
 
 export class AddUserRequest {
   @IsNotEmpty()
   @IsString()
   name: string;
+
+  toEntity() {
+    return User.of(this.name);
+  }
 }
