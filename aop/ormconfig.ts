d22c1879e62874e4356723d88724068f2dc667d9
sjk5766 <wjdrnr6662@gmail.com>
2025-01-08 12:39:02 +0900

    chore: ormconfig 적용

---
+++ aop/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+export const connectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'aop',
+  logging: true,
+  synchronize: false,
+  entities: [__dirname + '/src/**/entities/*.{js,ts}'],
+  migrations: [__dirname + '/src/migrations/*.{js,ts}'],
+  migrationsTableName: 'migrations',
+};
+
+const dataSource = new DataSource(connectionOptions);
+export default dataSource;
