19f06ef66d71796cc1066d1472e80beb78b871de
sjk5766 <wjdrnr6662@gmail.com>
2025-01-08 13:12:59 +0900

    feat: user CRUD api 추가

--- aop/src/user/user.controller.ts
+++ aop/src/user/user.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { AddUserRequest } from './dto/add-user-request';
+import { UpdateNameRequest } from './dto/update-name-request';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get(':id')
+  async findUser(@Param('id') id: number) {
+    return this.userService.findOne(id);
+  }
+
+  @Post()
+  async addUser(@Body() request: AddUserRequest) {
+    return this.userService.add(request);
+  }
+
+  @Put()
+  async updateUserName(@Body() request: UpdateNameRequest) {
+    return this.userService.updateName(request.id, request.name);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.userService.remove(id);
+  }
+}
