8f9d05f1a93f807a52a54feb8f611a16a8deec31
sjk5766 <wjdrnr6662@gmail.com>
2025-01-08 12:50:13 +0900

    feat: user 서비스 조회,추가,수정,삭제 함수

--- aop/src/user/user.service.ts
+++ aop/src/user/user.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  async findOne(id: number) {}
+
+  async add(id: number) {}
+
+  async updateName(id: number, name: string) {}
+
+  async remove(id: number) {}
+}
