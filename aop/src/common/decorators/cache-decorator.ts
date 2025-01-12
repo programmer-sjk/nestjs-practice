5469af844e7af15b186ee2857419d5e7d5766a44
sjk5766 <wjdrnr6662@gmail.com>
2025-01-12 14:06:07 +0900

    refactor: 불필요 로직 제거

--- aop/src/common/decorators/cache-decorator.ts
+++ aop/src/common/decorators/cache-decorator.ts
@@ -1,13 +1,9 @@
-
 export function CacheDecorator({ key: string, ttl: number }) {
   return (target: any, propertyKey: string, descriptor: PropertyDescriptor) => {
     const methodRef = descriptor.value;
 
     descriptor.value = async function (...args: any[]) {
-      const now = Date.now();
-      const result = await methodRef.call(this, ...args);
-
-      return result;
+      return methodRef.call(this, ...args);
     };
   };
 }
