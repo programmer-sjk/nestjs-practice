b6705c67a2b436019507847bb53258e56753891e
sjk5766 <wjdrnr6662@gmail.com>
2025-01-17 13:32:28 +0900

    feat: calendar controller, service, module 추가

--- calendar/src/app.module.ts
+++ calendar/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { CalendarModule } from './calendar/calendar.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, CalendarModule],
   controllers: [AppController],
   providers: [AppService],
 })
