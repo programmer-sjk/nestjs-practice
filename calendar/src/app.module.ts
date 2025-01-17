6dae82521de4ed9481efd7155643b58fc4f986bc
sjk5766 <wjdrnr6662@gmail.com>
2025-01-17 13:33:48 +0900

    refactor: app controller, service 제거

--- calendar/src/app.module.ts
+++ calendar/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
 import { CalendarModule } from './calendar/calendar.module';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [UserModule, CalendarModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- calendar/test/calendar/calendar.controller.spec.ts
+++ calendar/test/calendar/calendar.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CalendarController } from './calendar.controller';
+import { CalendarController } from '../../src/calendar/calendar.controller';
 
 describe('CalendarController', () => {
   let controller: CalendarController;
