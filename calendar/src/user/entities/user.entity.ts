ba81ba5ee36fa0375f47d3429f312f6675de91f2
sjk5766 <wjdrnr6662@gmail.com>
2025-01-17 13:39:23 +0900

    feat: user 엔티티 추가

---
+++ calendar/src/user/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 16 })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
