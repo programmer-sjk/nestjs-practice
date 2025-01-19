6e66f66a3db2129ba0db3ae5c505b5c25ea09fb6
sjk5766 <wjdrnr6662@gmail.com>
2025-01-19 14:34:42 +0900

    test: calendar service 테스트 추가

--- calendar/test/calendar/calendar.service.spec.ts
+++ calendar/test/calendar/calendar.service.spec.ts
@@ -1,18 +1,204 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CalendarService } from './calendar.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  initializeTransactionalContext,
+  StorageDriver,
+} from 'typeorm-transactional';
+import { CalendarService } from '../../src/calendar/calendar.service';
+import { RegisterCalendarRequest } from '../../src/calendar/dto/register-calendar.request';
+import { UpdateCalendarRequest } from '../../src/calendar/dto/update-calendar.request';
+import { CalendarUser } from '../../src/calendar/entities/calendar-user.entity';
+import { CalendarUserRepository } from '../../src/calendar/repositories/calendar-user.repository';
+import { CalendarRepository } from '../../src/calendar/repositories/calendar.repository';
+import { UserRepository } from '../../src/user/user.repository';
+import { TestCalendarFactory } from '../fixture/test-calendar-factory';
+import { TestUserFactory } from '../fixture/test-user-factory';
+import { testTypeormOptions } from '../test-ormconfig';
 
 describe('CalendarService', () => {
+  let module: TestingModule;
   let service: CalendarService;
+  let repository: CalendarRepository;
+  let calendarUserRepository: CalendarUserRepository;
+  let userRepository: UserRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CalendarService],
+  beforeAll(async () => {
+    initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+    module = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRootAsync(testTypeormOptions)],
+      providers: [
+        CalendarService,
+        CalendarRepository,
+        CalendarUserRepository,
+        UserRepository,
+      ],
     }).compile();
+    // addTransactionalDataSource(new DataSource(testConnectionOptions));
 
     service = module.get<CalendarService>(CalendarService);
+    repository = module.get<CalendarRepository>(CalendarRepository);
+    calendarUserRepository = module.get<CalendarUserRepository>(
+      CalendarUserRepository,
+    );
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  beforeEach(async () => {
+    await repository.clear();
+    await userRepository.clear();
+    await calendarUserRepository.clear();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('findAll', () => {
+    it('일정 목록을 조회할 수 있다.', async () => {
+      // given
+      const userA = await userRepository.save(TestUserFactory.of('서정국'));
+      const userB = await userRepository.save(TestUserFactory.of('계엄군'));
+
+      const calendar = await repository.save(
+        TestCalendarFactory.of('계엄령 회의'),
+      );
+
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userA.id));
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userB.id));
+
+      // when
+      const result = await service.findAll();
+
+      // then
+      expect(result[0].title).toBe('계엄령 회의');
+
+      const userIds = result[0].users.map((user) => user.id);
+      expect(userIds.sort()).toEqual([userA.id, userB.id].sort());
+    });
+  });
+
+  describe('addCalendar', () => {
+    it('일정을 등록할 수 있다.', async () => {
+      // given
+      const userA = await userRepository.save(TestUserFactory.of('서정국'));
+      const userB = await userRepository.save(TestUserFactory.of('계엄군'));
+
+      const dto = new RegisterCalendarRequest();
+      dto.title = '개발팀 회의';
+      dto.startDate = new Date('2025-01-31 15:00:00');
+      dto.endDate = new Date('2025-01-31 16:00:00');
+      dto.userIds = [userA.id, userB.id];
+
+      // when
+      await service.addCalendar(dto);
+
+      // then
+      const calendar = await repository.find();
+      expect(calendar[0].title).toBe('개발팀 회의');
+      expect(calendar[0].startDate).toEqual(new Date('2025-01-31 15:00:00'));
+      expect(calendar[0].endDate).toEqual(new Date('2025-01-31 16:00:00'));
+
+      const calendarUsers = await calendarUserRepository.find();
+      const userIds = calendarUsers.map((calendarUser) => calendarUser.userId);
+      expect(userIds.sort()).toEqual([userA.id, userB.id].sort());
+    });
+  });
+
+  describe('updateCalendar', () => {
+    it('일정을 수정할 수 있다.', async () => {
+      // given
+      const userA = await userRepository.save(TestUserFactory.of('서정국'));
+      const userB = await userRepository.save(TestUserFactory.of('계엄군'));
+
+      const calendar = await repository.save(
+        TestCalendarFactory.of('계엄령 회의'),
+      );
+
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userA.id));
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userB.id));
+
+      const dto = new UpdateCalendarRequest();
+      dto.title = '개발팀 회의';
+      dto.startDate = new Date('2025-01-31 15:00:00');
+      dto.endDate = new Date('2025-01-31 16:00:00');
+      dto.userIds = [userA.id, userB.id];
+
+      // when
+      await service.updateCalendar(dto);
+
+      // then
+      const updatedCalendar = await repository.findOneBy({ id: calendar.id });
+      expect(updatedCalendar.title).toBe('개발팀 회의');
+      expect(updatedCalendar.startDate).toEqual(
+        new Date('2025-01-31 15:00:00'),
+      );
+      expect(updatedCalendar.endDate).toEqual(new Date('2025-01-31 16:00:00'));
+    });
+
+    it('일정의 참석 사용자를 수정할 수 있다.', async () => {
+      // given
+      const userA = await userRepository.save(TestUserFactory.of('서정국'));
+      const userB = await userRepository.save(TestUserFactory.of('계엄군'));
+      const userC = await userRepository.save(TestUserFactory.of('개발팀'));
+
+      const calendar = await repository.save(
+        TestCalendarFactory.of('계엄령 회의'),
+      );
+
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userA.id));
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userB.id));
+
+      const dto = new UpdateCalendarRequest();
+      dto.title = '개발팀 회의';
+      dto.startDate = new Date('2025-01-31 15:00:00');
+      dto.endDate = new Date('2025-01-31 16:00:00');
+      dto.userIds = [userB.id, userC.id];
+
+      // when
+      await service.updateCalendar(dto);
+
+      // then
+      const updatedCalendar = await repository.findOneBy({ id: calendar.id });
+      expect(updatedCalendar.title).toBe('개발팀 회의');
+      expect(updatedCalendar.startDate).toEqual(
+        new Date('2025-01-31 15:00:00'),
+      );
+      expect(updatedCalendar.endDate).toEqual(new Date('2025-01-31 16:00:00'));
+
+      const updatedCalendarUsers = await calendarUserRepository.find();
+      const userIds = updatedCalendarUsers.map(
+        (calendarUser) => calendarUser.userId,
+      );
+      expect(userIds.sort()).toEqual([userB.id, userC.id].sort());
+    });
+  });
+
+  describe('removeCalendar', () => {
+    it('일정을 삭제할 수 있다.', async () => {
+      // given
+      const userA = await userRepository.save(TestUserFactory.of('서정국'));
+      const userB = await userRepository.save(TestUserFactory.of('계엄군'));
+
+      const calendar = await repository.save(
+        TestCalendarFactory.of('계엄령 회의'),
+      );
+
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userA.id));
+      await calendarUserRepository.save(CalendarUser.of(calendar.id, userB.id));
+
+      // when
+      await service.removeCalendar(calendar.id);
+
+      // then
+      const deletedCalendar = await repository.findOneBy({ id: calendar.id });
+      expect(deletedCalendar).toBeNull();
+
+      const calendarUsers = await calendarUserRepository.find();
+      expect(calendarUsers).toHaveLength(0);
+    });
   });
 });
