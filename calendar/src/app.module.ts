1851404f519f79cc601d25b8ca2d36889a56b7d3
sjk5766 <wjdrnr6662@gmail.com>
2025-01-20 13:01:43 +0900

    feat: notification 모듈과 서비스 추가

--- calendar/src/app.module.ts
+++ calendar/src/app.module.ts
@@ -3,12 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeormOptions } from '../ormconfig';
 import { CalendarModule } from './calendar/calendar.module';
 import { UserModule } from './user/user.module';
+import { NotificationModule } from './notification/notification.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync(typeormOptions),
     UserModule,
     CalendarModule,
+    NotificationModule,
   ],
 })
 export class AppModule {}
