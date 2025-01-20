4da5080abc38eb5b801edce74c7832eade52527e
sjk5766 <wjdrnr6662@gmail.com>
2025-01-20 13:36:57 +0900

    feat: 알림 타입에 따라 알림을 전송하는 기능 추가

--- calendar/src/notification/notification.module.ts
+++ calendar/src/notification/notification.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
 import { NotificationService } from './notification.service';
+import { MailNotificationService } from './services/mail-notification.service';
+import { PushNotificationService } from './services/push-notification.service';
+import { SmsNotificationService } from './services/sms-notification.service';
 
 @Module({
-  providers: [NotificationService]
+  providers: [
+    NotificationService,
+    MailNotificationService,
+    SmsNotificationService,
+    PushNotificationService,
+  ],
 })
 export class NotificationModule {}
