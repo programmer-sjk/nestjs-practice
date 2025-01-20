718acc799910e1b409f9a5eae88c444b63178d29
sjk5766 <wjdrnr6662@gmail.com>
2025-01-20 12:07:49 +0900

    feat: orphanedRowAction: 'delete' 옵션 추가

--- calendar/src/calendar/entities/calendar-user.entity.ts
+++ calendar/src/calendar/entities/calendar-user.entity.ts
@@ -15,6 +15,7 @@ export class CalendarUser {
 
   @ManyToOne(() => Calendar, (calendar) => calendar.calendarUsers, {
     createForeignKeyConstraints: false,
+    orphanedRowAction: 'delete',
   })
   calendar: Calendar;
 
