5216828528ca1ef57ecea2ab2846a68ec7415a44
sjk5766 <wjdrnr6662@gmail.com>
2025-01-21 16:21:48 +0900

    chore: user 마이그레이션 파일 추가

---
+++ graceful-shutdown/src/migrations/1737444050917-user.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class User1737444050917 implements MigrationInterface {
+  name = 'User1737444050917';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`user\` (
+        \`id\` int NOT NULL AUTO_INCREMENT, 
+        \`name\` varchar(16) NOT NULL, 
+        \`createdAt\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+        \`deletedAt\` datetime(6) NULL, 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`user\``);
+  }
+}
