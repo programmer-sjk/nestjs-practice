535c72cc7738e4ed0e6fcabaa295677fed931283
sjk5766 <wjdrnr6662@gmail.com>
2025-01-21 16:07:06 +0900

    feat: SIGTERM 수신 까지 종료되지 않는 long query API 추가

--- graceful-shutdown/src/user/user.service.ts
+++ graceful-shutdown/src/user/user.service.ts
@@ -1,4 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async doLongQuery() {
+    const user = new User();
+    user.name = 'before down';
+    const savedUser = await this.userRepository.save(user);
+    await this.delay(8);
+    savedUser.name = 'after down';
+    await this.userRepository.save(user);
+  }
+
+  private async delay(seconds: number) {
+    return new Promise((resolve) => setTimeout(resolve, seconds * 1000));
+  }
+}
