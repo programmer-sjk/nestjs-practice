38a2b63678b6f1a630886d948dabbdb4af185a36
sjk5766 <wjdrnr6662@gmail.com>
2025-01-23 16:29:41 +0900

    chore: coach 마이그레이션 파일 추가

---
+++ badminton/src/migrations/1737617327196-coach.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class Coach1737617327196 implements MigrationInterface {
+  name = 'Coach1737617327196';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`coach\` (
+        \`id\` int NOT NULL AUTO_INCREMENT, 
+        \`name\` varchar(255) NOT NULL, 
+        \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+        \`deleted_at\` datetime(6) NULL, 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE \`coach\``);
+  }
+}
