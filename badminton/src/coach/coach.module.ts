aa855340cccf263d46e0f2e7213b3f84b36cb9d2
sjk5766 <wjdrnr6662@gmail.com>
2025-01-24 13:56:22 +0900

    chore: coach 모듈의 imports 제거

--- badminton/src/coach/coach.module.ts
+++ badminton/src/coach/coach.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoachRepository } from './coach.repository';
-import { Coach } from './entities/coach.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Coach])],
   providers: [CoachRepository],
   exports: [CoachRepository],
 })
