10134691cff0fbf10bd8f7142ece24c0f7fbcc27
sjk5766 <wjdrnr6662@gmail.com>
2025-01-25 15:03:35 +0900

    feat: global prefix 추가

--- badminton/src/common/set-nest-app.ts
+++ badminton/src/common/set-nest-app.ts
@@ -1,11 +1,12 @@
 import {
   ClassSerializerInterceptor,
   INestApplication,
-  ValidationPipe,
+  ValidationPipe
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 export function setNestApp(app: INestApplication) {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.setGlobalPrefix('v1');
 }
