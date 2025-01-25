082a2067c0ff911d4f53529ed3cce59cfec43251
sjk5766 <wjdrnr6662@gmail.com>
2025-01-25 16:57:28 +0900

    feat: auth guards 추가

---
+++ badminton/src/auth/guards/auth.guard.ts
@@ -0,0 +1,53 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { Roles } from '../decorators/roles.decorator';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private readonly secretKey: string;
+
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly jwtService: JwtService,
+    private readonly reflector: Reflector,
+  ) {
+    this.secretKey = this.configService.get<string>('SECRET_KEY');
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.get(Roles, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: this.secretKey,
+      });
+      console.log(payload);
+      request['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
