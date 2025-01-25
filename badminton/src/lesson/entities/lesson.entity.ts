7eed053139bb8584931e2a74de2adff00a895780
sjk5766 <wjdrnr6662@gmail.com>
2025-01-25 15:59:48 +0900

    feat: lesson 엔티티 추가

---
+++ badminton/src/lesson/entities/lesson.entity.ts
@@ -0,0 +1,37 @@
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { DayOfWeek } from '../enums/day-of-week.enum';
+import { LessonType } from '../enums/lesson-type.enum';
+
+export class Lesson {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index('coachId')
+  @Column()
+  coachId: number;
+
+  @Index('userId')
+  @Column()
+  userId: number;
+
+  @Column({ type: 'enum', enum: LessonType })
+  type: LessonType;
+
+  @Column({ type: 'enum', enum: DayOfWeek })
+  dayOfWeek: DayOfWeek;
+
+  @Column()
+  startHour: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+}
