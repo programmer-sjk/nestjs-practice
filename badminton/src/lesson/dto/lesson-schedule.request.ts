ecdde648339724091ad96924b4d9b45b120796e4
sjk5766 <wjdrnr6662@gmail.com>
2025-01-30 14:23:01 +0900

    feat: 레슨 조회 dto에 Type 데코레이터 추가

--- badminton/src/lesson/dto/lesson-schedule.request.ts
+++ badminton/src/lesson/dto/lesson-schedule.request.ts
@@ -1,7 +1,9 @@
+import { Type } from 'class-transformer';
 import { IsInt, IsNotEmpty } from 'class-validator';
 
 export class LessonScheduleRequest {
   @IsNotEmpty()
+  @Type(() => Number)
   @IsInt()
   coachId: number;
 }
