e0c395d8b8b00531691acf80b142661c8cc7d71f
sjk5766 <wjdrnr6662@gmail.com>
2025-02-01 12:32:58 +0900

    feat: lesson 모듈에 redis 모듈 import

--- badminton/src/lesson/lesson.module.ts
+++ badminton/src/lesson/lesson.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CoachModule } from '../coach/coach.module';
+import { RedisModule } from '../redis/redis.module';
 import { LessonController } from './lesson.controller';
 import { LessonRepository } from './lesson.repository';
 import { LessonService } from './lesson.service';
 
 @Module({
-  imports: [CoachModule],
+  imports: [CoachModule, RedisModule],
   providers: [LessonService, LessonRepository],
   controllers: [LessonController],
 })
