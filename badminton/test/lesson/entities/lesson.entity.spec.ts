3fba7f4db3c8d6cde8ef20dfaa525cddded36b5b
sjk5766 <wjdrnr6662@gmail.com>
2025-02-07 15:20:01 +0900

    test: lesson 엔티티 테스트 케이스 추가

--- badminton/test/lesson/entities/lesson.entity.spec.ts
+++ badminton/test/lesson/entities/lesson.entity.spec.ts
@@ -1,3 +1,4 @@
+import { Lesson } from './../../../src/lesson/entities/lesson.entity';
 import { TestLessonCreator } from './../../fixture/entity/test-lesson-creator';
 
 describe('lesson', () => {
@@ -23,9 +24,23 @@ describe('lesson', () => {
     expect(result).toBe(true);
   });
 
-  describe('isInvalidLessonDate', () => {
+  describe('isInvalidLessonHour', () => {
     it.each([7, 8, 9, 12, 15, 21, 22])(
-      '레슨 시간이 올바르면 true를 반환한다.',
+      '레슨 시간이 올바르면 false를 반환한다.',
+      (lessonHour) => {
+        // given
+        const lesson = TestLessonCreator.of(lessonHour);
+
+        // when
+        const result = lesson.isInvalidLessonHour();
+
+        // then
+        expect(result).toBe(false);
+      },
+    );
+
+    it.each([23, 0, 1, 2, 3, 4, 5, 6])(
+      '레슨 시간이 올바르지 않으면 true를 반환한다.',
       (lessonHour) => {
         // given
         const lesson = TestLessonCreator.of(lessonHour);
@@ -37,15 +52,35 @@ describe('lesson', () => {
         expect(result).toBe(true);
       },
     );
+  });
 
-    it.each([23, 0, 1, 2, 3, 4, 5, 6])(
-      '레슨 시간이 올바르지 않으면 false를 반환한다.',
-      (lessonHour) => {
+  describe('isInvalidLessonDate', () => {
+    it.each(['2025-02-08', '2025-02-09', '2025-02-10', '2025-02-11'])(
+      '레슨 날짜가 올바르면 false를 반환한다.',
+      (startDate) => {
         // given
-        const lesson = TestLessonCreator.of(lessonHour);
+        jest.useFakeTimers().setSystemTime(new Date('2025-02-07'));
+
+        const lesson = new Lesson();
+        lesson.startDate = new Date(startDate);
 
         // when
-        const result = lesson.isInvalidLessonHour();
+        const result = lesson.isInvalidLessonDate();
+
+        // then
+        expect(result).toBe(false);
+      },
+    );
+
+    it.each(['2025-01-08', '2025-01-09', '2025-01-10', '2025-01-11'])(
+      '레슨 시간이 올바르지 않으면 false를 반환한다.',
+      (startDate) => {
+        // given
+        const lesson = new Lesson();
+        lesson.startDate = new Date(startDate);
+
+        // when
+        const result = lesson.isInvalidLessonDate();
 
         // then
         expect(result).toBe(true);
