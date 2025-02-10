bc13be6318b7aba925fad76162c8057bd18ed252
sjk5766 <wjdrnr6662@gmail.com>
2025-02-10 13:49:29 +0900

    test: lesson service 테스트 초기화

--- badminton/test/lesson/lesson.service.spec.ts
+++ badminton/test/lesson/lesson.service.spec.ts
@@ -1,15 +1,30 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { LessonService } from './lesson.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonModule } from '../../src/lesson/lesson.module';
+import { LessonRepository } from '../../src/lesson/lesson.repository';
+import { LessonService } from '../../src/lesson/lesson.service';
+import { testConnectionOptions } from '../test-ormconfig';
 
 describe('LessonService', () => {
+  let module: TestingModule;
   let service: LessonService;
+  let lessonRepository: LessonRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [LessonService],
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(testConnectionOptions), LessonModule],
     }).compile();
 
     service = module.get<LessonService>(LessonService);
+    lessonRepository = module.get<LessonRepository>(LessonRepository);
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  beforeEach(async () => {
+    await lessonRepository.clear();
   });
 
   it('should be defined', () => {
