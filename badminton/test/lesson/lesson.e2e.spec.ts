6719565b1cff76c219e9d6d242967176a7a39eea
sjk5766 <wjdrnr6662@gmail.com>
2025-02-11 16:38:54 +0900

    test: 레슨 e2e 예약 테스트 추가

--- badminton/test/lesson/lesson.e2e.spec.ts
+++ badminton/test/lesson/lesson.e2e.spec.ts
@@ -4,8 +4,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as request from 'supertest';
 import { CoachRepository } from '../../src/coach/coach.repository';
+import { DateUtil } from '../../src/common/date-util';
 import { setNestApp } from '../../src/common/set-nest-app';
 import { LessonScheduleRequest } from '../../src/lesson/dto/lesson-schedule.request';
+import { RegisterRequest } from '../../src/lesson/dto/register.request';
+import { DayOfWeek } from '../../src/lesson/enums/day-of-week.enum';
+import { LessonType } from '../../src/lesson/enums/lesson-type.enum';
 import { LessonController } from '../../src/lesson/lesson.controller';
 import { LessonModule } from '../../src/lesson/lesson.module';
 import { LessonRepository } from '../../src/lesson/lesson.repository';
@@ -66,6 +70,35 @@ describe('LessonController', () => {
       const schedules = response.body.data.schedules;
       expect(Object.entries(schedules)).toHaveLength(7);
       expect(Object.entries(schedules)[0][1]).toHaveLength(16);
+    });
+  });
+
+  describe('POST /v1/lesson', () => {
+    it('레슨을 등록할 수 있다.', async () => {
+      // given
+      const coach = await coachRepository.save(TestCoachCreator.of());
+      const requestDto = new RegisterRequest();
+      requestDto.coachId = coach.id;
+      requestDto.userId = 1;
+      requestDto.type = LessonType.ONE_TIME;
+      requestDto.dayOfWeek = DayOfWeek.MONDAY;
+      requestDto.startDate = DateUtil.now()
+        .plus({ day: 1 })
+        .set({ hour: 7 })
+        .toJSDate();
+
+      // when
+      await request(app.getHttpServer())
+        .post('/v1/lesson')
+        .send(requestDto)
+        .expect(HttpStatus.CREATED);
+
+      // then
+      const result = await lessonRepository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].coachId).toBe(coach.id);
+      expect(result[0].userId).toBe(1);
+      expect(result[0].type).toBe(LessonType.ONE_TIME);
     });
   });
 });
