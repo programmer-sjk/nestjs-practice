33654b263188906068f25719054750d179b25a9c
sjk5766 <wjdrnr6662@gmail.com>
2025-02-11 16:12:56 +0900

    test: 레슨을 추가하는 서비스 테스트 코드 추가

--- badminton/test/lesson/lesson.service.spec.ts
+++ badminton/test/lesson/lesson.service.spec.ts
@@ -1,7 +1,13 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoachRepository } from '../../src/coach/coach.repository';
+import { DateUtil } from '../../src/common/date-util';
 import { LessonScheduleRequest } from '../../src/lesson/dto/lesson-schedule.request';
+import { RegisterRequest } from '../../src/lesson/dto/register.request';
+import { Lesson } from '../../src/lesson/entities/lesson.entity';
+import { DayOfWeek } from '../../src/lesson/enums/day-of-week.enum';
+import { LessonType } from '../../src/lesson/enums/lesson-type.enum';
 import { LessonModule } from '../../src/lesson/lesson.module';
 import { LessonRepository } from '../../src/lesson/lesson.repository';
 import { LessonService } from '../../src/lesson/lesson.service';
@@ -50,6 +56,97 @@ describe('LessonService', () => {
       // then
       expect(Object.entries(result.schedules)).toHaveLength(7);
       expect(Object.entries(result.schedules)[0][1]).toHaveLength(16);
+    });
+  });
+
+  describe('addLesson', () => {
+    it('레슨을 등록할 수 있다', async () => {
+      // given
+      const coach = await coachRepository.save(TestCoachCreator.of());
+      const dto = new RegisterRequest();
+      dto.coachId = coach.id;
+      dto.userId = 1;
+      dto.type = LessonType.ONE_TIME;
+      dto.dayOfWeek = DayOfWeek.MONDAY;
+      dto.startDate = DateUtil.now()
+        .plus({ day: 1 })
+        .set({ hour: 7 })
+        .toJSDate();
+
+      // when
+      await service.addLesson(dto);
+
+      // then
+      const result = await lessonRepository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].coachId).toBe(coach.id);
+      expect(result[0].userId).toBe(1);
+      expect(result[0].type).toBe(LessonType.ONE_TIME);
+    });
+
+    it('레슨 시간이 유효하지 않으면 등록할 수 없다', async () => {
+      // given
+      const coach = await coachRepository.save(TestCoachCreator.of());
+      const dto = new RegisterRequest();
+      dto.coachId = coach.id;
+      dto.userId = 1;
+      dto.type = LessonType.ONE_TIME;
+      dto.dayOfWeek = DayOfWeek.MONDAY;
+      dto.startDate = DateUtil.now()
+        .plus({ day: 1 })
+        .set({ hour: 6 })
+        .toJSDate();
+
+      // when & then
+      await expect(service.addLesson(dto)).rejects.toThrow(
+        new BadRequestException('예약할 수 없는 시간입니다.'),
+      );
+    });
+
+    it('레슨 날짜가 유효하지 않으면 등록할 수 없다', async () => {
+      // given
+      const coach = await coachRepository.save(TestCoachCreator.of());
+      const dto = new RegisterRequest();
+      dto.coachId = coach.id;
+      dto.userId = 1;
+      dto.type = LessonType.ONE_TIME;
+      dto.dayOfWeek = DayOfWeek.MONDAY;
+      dto.startDate = DateUtil.now().set({ hour: 7 }).toJSDate();
+
+      // when & then
+      await expect(service.addLesson(dto)).rejects.toThrow(
+        new BadRequestException('예약은 내일부터 일주일간 가능합니다.'),
+      );
+    });
+
+    it('이미 예약된 시간에 레슨을 등록할 수 없다.', async () => {
+      // given
+      const coach = await coachRepository.save(TestCoachCreator.of());
+      const startDate = DateUtil.now()
+        .plus({ day: 1 })
+        .set({ hour: 7 })
+        .toJSDate();
+      const existLesson = Lesson.of(
+        coach.id,
+        1,
+        LessonType.ONE_TIME,
+        DayOfWeek.MONDAY,
+        undefined,
+        startDate,
+      );
+      await lessonRepository.save(existLesson);
+
+      const dto = new RegisterRequest();
+      dto.coachId = coach.id;
+      dto.userId = 1;
+      dto.type = LessonType.ONE_TIME;
+      dto.dayOfWeek = DayOfWeek.MONDAY;
+      dto.startDate = startDate;
+
+      // when & then
+      await expect(service.addLesson(dto)).rejects.toThrow(
+        new BadRequestException('이미 예약된 레슨 시간입니다.'),
+      );
     });
   });
 });
