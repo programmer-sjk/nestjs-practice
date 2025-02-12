455a23c45342a2da07fa90a82a3e772694975425
sjk5766 <wjdrnr6662@gmail.com>
2025-02-12 14:58:32 +0900

    feat: product 관련 모듈 추가

--- product/src/app.module.ts
+++ product/src/app.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { AdminModule } from './admin/admin.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { PointModule } from './point/point.module';
+import { CouponModule } from './coupon/coupon.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, AdminModule, ProductModule, OrderModule, PointModule, CouponModule],
 })
 export class AppModule {}
