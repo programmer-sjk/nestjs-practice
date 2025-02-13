aadd459e8260d681d10238c68e226271ea59fb1c
sjk5766 <wjdrnr6662@gmail.com>
2025-02-13 14:23:21 +0900

    chore: user, admin의 email 컬럼에 unique index 추가해서 마이그레이션 파일 생성

--- product/src/migrations/1739424090304-user.ts
+++ product/src/migrations/1739424090304-user.ts
@@ -1,21 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Admin1739419158712 implements MigrationInterface {
-  name = 'Admin1739419158712';
+export class User1739424090304 implements MigrationInterface {
+  name = 'User1739424090304';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`admin\` (
+      `CREATE TABLE \`user\` (
         \`id\` int NOT NULL AUTO_INCREMENT, 
         \`email\` varchar(16) NOT NULL, 
         \`password\` varchar(255) NOT NULL, 
         \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
         \`deleted_at\` datetime(6) NULL, 
+        UNIQUE INDEX \`email\` (\`email\`), 
         PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`admin\``);
+    await queryRunner.query(`DROP INDEX \`email\` ON \`user\``);
+    await queryRunner.query(`DROP TABLE \`user\``);
   }
 }
--- product/src/migrations/1739424145528-admin.ts
+++ product/src/migrations/1739424145528-admin.ts
@@ -1,20 +1,23 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class User1739418729447 implements MigrationInterface {
-  name = 'User1739418729447';
+export class Admin1739424145528 implements MigrationInterface {
+  name = 'Admin1739424145528';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `CREATE TABLE \`user\` (
+      `CREATE TABLE \`admin\` (
         \`id\` int NOT NULL AUTO_INCREMENT, 
         \`email\` varchar(16) NOT NULL, 
         \`password\` varchar(255) NOT NULL, 
         \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
-        \`deleted_at\` datetime(6) NULL, PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+        \`deleted_at\` datetime(6) NULL, 
+        UNIQUE INDEX \`email\` (\`email\`), 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP TABLE \`user\``);
+    await queryRunner.query(`DROP INDEX \`email\` ON \`admin\``);
+    await queryRunner.query(`DROP TABLE \`admin\``);
   }
 }
