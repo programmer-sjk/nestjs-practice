b0d1525f053c3db3531b5a4b9c6ac1ee99aaa7e8
sjk5766 <wjdrnr6662@gmail.com>
2025-02-13 13:36:43 +0900

    test: SignUp dto 테스트 추가

---
+++ product/test/user/dto/sign-up.request.spec.ts
@@ -0,0 +1,39 @@
+import { validate } from 'class-validator';
+import { SignUpRequest } from '../../../src/user/dto/sign-up.request';
+
+describe('SignUpRequest', () => {
+  it('email은 문자열로 최대 16글자로 필수 값이다.', async () => {
+    // given
+    const dto = new SignUpRequest();
+    dto.email = undefined;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    const emailErrors = errors.find((err) => err.property === 'email');
+    expect(emailErrors.constraints).toHaveProperty('isString');
+    expect(emailErrors.constraints).toHaveProperty('maxLength');
+    expect(emailErrors.constraints).toHaveProperty('isNotEmpty');
+
+    const constraintsKeys = Object.keys(emailErrors.constraints);
+    expect(constraintsKeys).toHaveLength(3);
+  });
+
+  it('password는 문자열로 필수 값이다.', async () => {
+    // given
+    const dto = new SignUpRequest();
+    dto.password = undefined;
+
+    // when
+    const errors = await validate(dto);
+
+    // then
+    const passwordErrors = errors.find((err) => err.property === 'password');
+    expect(passwordErrors.constraints).toHaveProperty('isString');
+    expect(passwordErrors.constraints).toHaveProperty('isNotEmpty');
+
+    const constraintsKeys = Object.keys(passwordErrors.constraints);
+    expect(constraintsKeys).toHaveLength(2);
+  });
+});
