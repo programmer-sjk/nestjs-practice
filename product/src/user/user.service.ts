c70a5cd8c744ca0aeca056e4f3636fd284013342
sjk5766 <wjdrnr6662@gmail.com>
2025-02-13 14:17:13 +0900

    feat: email로 user를 조회하는 메서드 추가

--- product/src/user/user.service.ts
+++ product/src/user/user.service.ts
@@ -6,6 +6,10 @@ import { UserRepository } from './user.repository';
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
+  async findOne(email: string) {
+    return this.userRepository.findOneBy({ email });
+  }
+
   async addUser(dto: SignUpRequest) {
     await this.userRepository.save(dto.toEntity());
   }
