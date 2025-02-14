095eb51ee96b0234083fc621606f0a358197561b
sjk5766 <wjdrnr6662@gmail.com>
2025-02-14 13:36:59 +0900

    feat: app 모듈에 typeorm 설정 추가

--- product/src/app.module.ts
+++ product/src/app.module.ts
@@ -1,13 +1,24 @@
 import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../ormconfig';
 import { AdminModule } from './admin/admin.module';
-import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { CouponModule } from './coupon/coupon.module';
 import { OrderModule } from './order/order.module';
 import { PointModule } from './point/point.module';
-import { CouponModule } from './coupon/coupon.module';
-import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, AdminModule, ProductModule, OrderModule, PointModule, CouponModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions),
+    UserModule,
+    AdminModule,
+    ProductModule,
+    OrderModule,
+    PointModule,
+    CouponModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
