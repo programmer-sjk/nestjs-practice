de59d35601c43360300073711379732f5e5d888e
sjk5766 <wjdrnr6662@gmail.com>
2025-02-14 14:57:47 +0900

    refactor: 카테고리 spec 파일을 test 하위 디렉터리로 이동

--- product/test/category/category.controller.spec.ts
+++ product/test/category/category.controller.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CategoryController } from './category.controller';
+import { CategoryController } from '../../src/category/category.controller';
 
 describe('CategoryController', () => {
   let controller: CategoryController;
