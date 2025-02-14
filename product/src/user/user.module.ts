24b4f11b1acc825067966f93be80be0ab7416a4f
sjk5766 <wjdrnr6662@gmail.com>
2025-02-14 13:39:56 +0900

    feat: UserModule에 exports에 UserService 노출

--- product/src/user/user.module.ts
+++ product/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
+  controllers: [UserController],
   providers: [UserService, UserRepository],
-  controllers: [UserController]
+  exports: [UserService],
 })
 export class UserModule {}
