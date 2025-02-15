767d5bb2575222a0e7c7ff38addc706fd4e4d459
sjk5766 <wjdrnr6662@gmail.com>
2025-02-15 20:19:07 +0900

    feat: admin 회원가입 기능

--- product/src/admin/admin.controller.ts
+++ product/src/admin/admin.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ResponseEntity } from '../common/response-entity';
+import { AdminService } from './admin.service';
+import { AdminSignUpRequest } from './dto/admin-sign-up.request';
 
 @Controller('admin')
-export class AdminController {}
+export class AdminController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post()
+  adminSignUp(@Body() request: AdminSignUpRequest) {
+    await this.adminService.signUp(request)
+    return ResponseEntity.OK();
+  }
+}
--- product/src/admin/admin.service.ts
+++ product/src/admin/admin.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { SignUpRequest } from '../user/dto/sign-up.request';
+import { AdminRepository } from './admin.repository';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(private readonly adminRepository: AdminRepository) {}
+
+  async signUp(dto: SignUpRequest) {
+    await this.adminRepository.save(dto.toEntity());
+  }
+}
