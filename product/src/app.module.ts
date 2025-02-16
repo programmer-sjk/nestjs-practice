65c5e0d9e288c3d948d33174b5b2c8a00b64ebb6
sjk5766 <wjdrnr6662@gmail.com>
2025-02-16 16:07:39 +0900

    feat: AuthGuard 글로벌로 동작하도록 app 모듈의 provider에 등록

--- product/src/app.module.ts
+++ product/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { CategoryModule } from './category/category.module';
 import { CouponModule } from './coupon/coupon.module';
 import { OrderModule } from './order/order.module';
 import { PointModule } from './point/point.module';
 import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
-import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
@@ -21,6 +25,14 @@ import { CategoryModule } from './category/category.module';
     CouponModule,
     AuthModule,
     CategoryModule,
+    ConfigModule,
+    JwtModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule {}
