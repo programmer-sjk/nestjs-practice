c56e25e7972cf438a1736c2b3c9b23c2ec4219d5
sjk5766 <wjdrnr6662@gmail.com>
2025-02-16 16:08:31 +0900

    feat: 카테고리 등록 API에 Admin Role 데코레이터 적용

--- product/src/category/category.controller.ts
+++ product/src/category/category.controller.ts
@@ -1,12 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { CategoryService } from './category.service';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 import { ResponseEntity } from './../common/response-entity';
+import { CategoryService } from './category.service';
 import { RegisterRequest } from './dto/register.request';
 
 @Controller('category')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
+  @Roles(Role.ADMIN)
   @Post()
   async register(@Body() request: RegisterRequest) {
     await this.categoryService.addCategory(request);
