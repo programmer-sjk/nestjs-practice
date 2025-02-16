38d2c7856fa35ebb49176e71dc1f85e442b2ea37
sjk5766 <wjdrnr6662@gmail.com>
2025-02-16 16:15:36 +0900

    test: 카테고리 추가 기능에 대한 service 테스트 함수 추가

--- product/test/category/category.service.spec.ts
+++ product/test/category/category.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { CategoryService } from './category.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryModule } from '../../src/category/category.module';
+import { CategoryRepository } from '../../src/category/category.repository';
+import { CategoryService } from '../../src/category/category.service';
+import { CategoryRegisterRequest } from '../../src/category/dto/category-register.request';
+import { testConnectionOptions } from '../test-ormconfig';
 
 describe('CategoryService', () => {
+  let module: TestingModule;
   let service: CategoryService;
+  let repository: CategoryRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CategoryService],
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(testConnectionOptions), CategoryModule],
     }).compile();
 
     service = module.get<CategoryService>(CategoryService);
+    repository = module.get<CategoryRepository>(CategoryRepository);
   });
 
+  beforeEach(async () => {
+    await repository.clear();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addCategory', () => {
+    it('카테고리를 추가할 수 있다.', async () => {
+      // given
+      const dto = new CategoryRegisterRequest();
+      dto.name = '의류';
+
+      // when
+      await service.addCategory(dto);
+
+      // then
+      const result = await repository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].name).toBe('의류');
+    });
   });
 });
