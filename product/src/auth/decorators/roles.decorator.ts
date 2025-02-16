ac873c2208282324e17ba91311883d7120ed70f4
sjk5766 <wjdrnr6662@gmail.com>
2025-02-16 16:07:56 +0900

    feat: role guard 추가

--- product/src/auth/decorators/roles.decorator.ts
+++ product/src/auth/decorators/roles.decorator.ts
@@ -1,5 +1,4 @@
 import { SetMetadata } from '@nestjs/common';
-import { Role } from '../enums/role.enum';
 
 export const ROLE_KEY = 'roles';
-export const Roles = () => SetMetadata(ROLE_KEY, [Role.ADMIN, Role.USER]);
+export const Roles = (Role) => SetMetadata(ROLE_KEY, Role);
