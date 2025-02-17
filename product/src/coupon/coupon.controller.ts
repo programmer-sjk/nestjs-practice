bb5ec0036ccd98e49d5f5ccf0f165f200f68ec45
sjk5766 <wjdrnr6662@gmail.com>
2025-02-17 15:02:40 +0900

    feat: coupon 등록 기능에 어드민 권한 추가

--- product/src/coupon/coupon.controller.ts
+++ product/src/coupon/coupon.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 import { ResponseEntity } from '../common/response-entity';
 import { CouponService } from './coupon.service';
 import { CouponRegisterRequest } from './dto/coupon-register.request';
@@ -7,6 +9,7 @@ import { CouponRegisterRequest } from './dto/coupon-register.request';
 export class CouponController {
   constructor(private readonly couponService: CouponService) {}
 
+  @Roles(Role.ADMIN)
   @Post()
   async reigster(@Body() request: CouponRegisterRequest) {
     await this.couponService.addCoupon(request);
