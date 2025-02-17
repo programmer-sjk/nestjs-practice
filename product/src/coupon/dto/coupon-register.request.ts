50ae7036a2f9ce2275d4ffa156bbf39710f6a717
sjk5766 <wjdrnr6662@gmail.com>
2025-02-17 15:02:01 +0900

    feat: coupon 등록 dto 추가

---
+++ product/src/coupon/dto/coupon-register.request.ts
@@ -0,0 +1,42 @@
+import {
+  IsEnum,
+  IsInt,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+} from 'class-validator';
+import { Coupon } from '../entities/coupon.entity';
+import { CouponType } from '../enums/coupon-type.enum';
+
+export class CouponRegisterRequest {
+  @IsNotEmpty()
+  @MaxLength(32)
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEnum(CouponType)
+  type: CouponType;
+
+  @IsNotEmpty()
+  @IsInt()
+  value: number;
+
+  @IsNotEmpty()
+  @IsInt()
+  stock: number;
+
+  @IsNotEmpty()
+  @IsString()
+  description: string;
+
+  toEntity() {
+    return Coupon.of(
+      this.name,
+      this.type,
+      this.value,
+      this.stock,
+      this.description,
+    );
+  }
+}
