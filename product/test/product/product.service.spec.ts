eb3f690d651d0e23b7e04ad5c92c05f38bf936e1
sjk5766 <wjdrnr6662@gmail.com>
2025-02-17 14:44:04 +0900

    test: 상품 등록 기능 service 테스트 코드 추가

--- product/test/product/product.service.spec.ts
+++ product/test/product/product.service.spec.ts
@@ -1,18 +1,54 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductRegisterRequest } from '../../src/product/dto/product-register.request';
+import { ProductModule } from '../../src/product/product.module';
+import { ProductRepository } from '../../src/product/product.repository';
 import { ProductService } from '../../src/product/product.service';
+import { testConnectionOptions } from '../test-ormconfig';
 
 describe('ProductService', () => {
+  let module: TestingModule;
   let service: ProductService;
+  let repository: ProductRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ProductService],
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(testConnectionOptions), ProductModule],
     }).compile();
 
     service = module.get<ProductService>(ProductService);
+    repository = module.get<ProductRepository>(ProductRepository);
+  });
+
+  beforeEach(async () => {
+    await repository.clear();
+  });
+
+  afterAll(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addProcut', () => {
+    it('상품을 등록할 수 있다.', async () => {
+      // given
+      const dto = new ProductRegisterRequest();
+      dto.name = '검은색 패딩';
+      dto.price = 100_000;
+      dto.stock = 50;
+
+      // when
+      await service.addProduct(dto);
+
+      // then
+      const result = await repository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].name).toBe('검은색 패딩');
+      expect(result[0].price).toBe(100_000);
+      expect(result[0].stock).toBe(50);
+    });
   });
 });
