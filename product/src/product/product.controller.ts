8c55d02a177d5c36b85c73c051cf3ed35cbb8ff1
sjk5766 <wjdrnr6662@gmail.com>
2025-02-17 14:09:31 +0900

    feat: product 등록 기능

--- product/src/product/product.controller.ts
+++ product/src/product/product.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ResponseEntity } from './../common/response-entity';
+import { ProductRegisterRequest } from './dto/product-register.request';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async register(@Body() request: ProductRegisterRequest) {
+    await this.productService.addProduct(request);
+    return ResponseEntity.OK();
+  }
+}
--- product/src/product/product.service.ts
+++ product/src/product/product.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRegisterRequest } from './dto/product-register.request';
+import { ProductRepository } from './product.repository';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly productRepository: ProductRepository) {}
+  async addProduct(dto: ProductRegisterRequest) {
+    await this.productRepository.save(dto.toEntity());
+  }
+}
