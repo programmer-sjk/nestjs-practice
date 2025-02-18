f5769ed5049d421209608709a3a721ffd6ac09b9
sjk5766 <wjdrnr6662@gmail.com>
2025-02-18 14:55:34 +0900

    chore: test용 ormconfig 파일에도 typeorm-transactional 설정 추가

--- product/test/test-ormconfig.ts
+++ product/test/test-ormconfig.ts
@@ -1,5 +1,7 @@
-import { DataSourceOptions } from 'typeorm';
+import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+import { addTransactionalDataSource } from 'typeorm-transactional';
 
 export const testConnectionOptions: DataSourceOptions = {
   type: 'mysql',
@@ -14,4 +16,12 @@ export const testConnectionOptions: DataSourceOptions = {
   migrations: [__dirname + '/../src/migrations/*{.ts,.js}'],
   migrationsTableName: 'migrations',
   namingStrategy: new SnakeNamingStrategy(),
+};
+
+export const testTypeormOptions: TypeOrmModuleAsyncOptions = {
+  useFactory: () => testConnectionOptions,
+  async dataSourceFactory(option) {
+    if (!option) throw new Error('Invalid typeorm options passed');
+    return addTransactionalDataSource(new DataSource(option));
+  },
 };
