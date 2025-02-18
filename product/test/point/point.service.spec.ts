096eaa4d0098d69fc0d7007d3a6433961ad22762
sjk5766 <wjdrnr6662@gmail.com>
2025-02-18 13:05:04 +0900

    test: 포인트 지급 기능에 대한 service 테스트 코드 작성

--- product/test/point/point.service.spec.ts
+++ product/test/point/point.service.spec.ts
@@ -1,18 +1,58 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { PointService } from './point.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PointModule } from '../../src/point/point.module';
+import { PointRepository } from '../../src/point/point.repository';
+import { PointService } from '../../src/point/point.service';
+import { UserModule } from '../../src/user/user.module';
+import { UserRepository } from '../../src/user/user.repository';
+import { UserFactory } from '../fixture/entities/user-factory';
+import { testConnectionOptions } from '../test-ormconfig';
 
 describe('PointService', () => {
+  let module: TestingModule;
   let service: PointService;
+  let repository: PointRepository;
+  let userRepository: UserRepository;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [PointService],
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot(testConnectionOptions),
+        PointModule,
+        UserModule,
+      ],
     }).compile();
 
     service = module.get<PointService>(PointService);
+    repository = module.get<PointRepository>(PointRepository);
+    userRepository = module.get<UserRepository>(UserRepository);
+  });
+
+  beforeEach(async () => {
+    await repository.clear();
+    await userRepository.clear();
+  });
+
+  afterAll(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('addPointToUser', () => {
+    it('사용자에게 포인트를 지급할 수 있다.', async () => {
+      // given
+      const user = await userRepository.save(UserFactory.of());
+      const pointValue = 1_000;
+
+      // when
+      await service.addPointToUser(user.id, pointValue);
+
+      // then
+      const point = await repository.findOneBy({ userId: user.id });
+      expect(point.value).toBe(1_000);
+    });
   });
 });
