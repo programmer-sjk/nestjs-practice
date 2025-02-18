54b6a9ad4e563214b178cc4b2cceba450b411b93
sjk5766 <wjdrnr6662@gmail.com>
2025-02-18 12:59:57 +0900

    feat: 포인트 지급시 사용자가 존재하는지 체크하는 기능 추가

--- product/src/point/point.module.ts
+++ product/src/point/point.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { PointController } from './point.controller';
 import { PointRepository } from './point.repository';
 import { PointService } from './point.service';
 
 @Module({
+  imports: [UserModule],
   providers: [PointService, PointRepository],
   controllers: [PointController],
 })
--- product/src/point/point.service.ts
+++ product/src/point/point.service.ts
@@ -1,12 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UserService } from './../user/user.service';
 import { Point } from './entities/point.entity';
 import { PointRepository } from './point.repository';
 
 @Injectable()
 export class PointService {
-  constructor(private readonly pointRepository: PointRepository) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly pointRepository: PointRepository,
+  ) {}
 
   async addPointToUser(userId: number, value: number) {
+    const user = await this.userService.findOneById(userId);
+    if (!user) {
+      throw new BadRequestException('존재하지 않는 사용자입니다.');
+    }
+
     await this.pointRepository.save(Point.of(userId, value));
   }
 }
