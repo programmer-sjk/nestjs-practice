61ffd03ae0506b1c8190d18e99794c253079697e
sjk5766 <wjdrnr6662@gmail.com>
2025-02-18 14:27:53 +0900

    feat: app 모듈과 main 함수에 typeorm-transactional 설정 적용

--- product/src/app.module.ts
+++ product/src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { addTransactionalDataSource } from 'typeorm-transactional';
 import { connectionOptions } from '../ormconfig';
 import { AdminModule } from './admin/admin.module';
 import { AuthModule } from './auth/auth.module';
@@ -16,7 +18,18 @@ import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(connectionOptions),
+    TypeOrmModule.forRootAsync({
+      useFactory() {
+        return connectionOptions;
+      },
+      async dataSourceFactory(options) {
+        if (!options) {
+          throw new Error('Invalid options passed');
+        }
+
+        return addTransactionalDataSource(new DataSource(options));
+      },
+    }),
     UserModule,
     AdminModule,
     ProductModule,
--- product/src/main.ts
+++ product/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  initializeTransactionalContext,
+  StorageDriver,
+} from 'typeorm-transactional';
 import { AppModule } from './app.module';
 import { setNestApp } from './common/set-nest-app';
 
 async function bootstrap() {
+  initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
+  
   const app = await NestFactory.create(AppModule);
   setNestApp(app);
   await app.listen(3000);
