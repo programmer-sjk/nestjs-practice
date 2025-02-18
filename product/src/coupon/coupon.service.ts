628ea045b2f5a0a8609594a69fb5f077d52d1ff5
sjk5766 <wjdrnr6662@gmail.com>
2025-02-18 14:19:24 +0900

    fix: 회원가입시 coupon이 아닌 coupon-user에 저장되도록 수정

--- product/src/coupon/coupon.service.ts
+++ product/src/coupon/coupon.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { CouponRegisterRequest } from './dto/coupon-register.request';
 import { CouponUser } from './entities/coupon-user.entity';
+import { CouponUserRepository } from './repositories/coupon-user.repository';
 import { CouponRepository } from './repositories/coupon.repository';
 
 @Injectable()
 export class CouponService {
   private readonly SIGN_UP_COUPON_NAME = '신규회원 쿠폰';
 
-  constructor(private readonly couponRepository: CouponRepository) {}
+  constructor(
+    private readonly couponRepository: CouponRepository,
+    private readonly couponUserRepository: CouponUserRepository,
+  ) {}
 
   async addCoupon(dto: CouponRegisterRequest) {
     await this.couponRepository.save(dto.toEntity());
@@ -17,6 +21,6 @@ export class CouponService {
     const coupon = await this.couponRepository.findOneBy({
       name: this.SIGN_UP_COUPON_NAME,
     });
-    await this.couponRepository.save(CouponUser.of(coupon.id, userId));
+    await this.couponUserRepository.save(CouponUser.of(coupon.id, userId));
   }
 }
