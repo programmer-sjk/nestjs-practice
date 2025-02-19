bf42569224839f0e5934c793a19def7b158e29af
sjk5766 <wjdrnr6662@gmail.com>
2025-02-19 14:18:51 +0900

    test: 신규 회원 쿠폰 발급 메서드에 대한 service 테스트 추가

--- product/test/coupon/coupon.service.spec.ts
+++ product/test/coupon/coupon.service.spec.ts
@@ -1,31 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CouponModule } from '../../src/coupon/coupon.module';
-import { CouponRepository } from '../../src/coupon/repositories/coupon.repository';
 import { CouponService } from '../../src/coupon/coupon.service';
 import { CouponRegisterRequest } from '../../src/coupon/dto/coupon-register.request';
 import { CouponType } from '../../src/coupon/enums/coupon-type.enum';
-import { testConnectionOptions } from '../test-ormconfig';
 import { CouponUserRepository } from '../../src/coupon/repositories/coupon-user.repository';
+import { CouponRepository } from '../../src/coupon/repositories/coupon.repository';
+import { UserModule } from '../../src/user/user.module';
+import { UserRepository } from '../../src/user/user.repository';
+import { CouponFactory } from '../fixture/entities/coupon-factory';
+import { UserFactory } from '../fixture/entities/user-factory';
+import { testConnectionOptions } from '../test-ormconfig';
 
 describe('CouponService', () => {
   let module: TestingModule;
   let service: CouponService;
   let repository: CouponRepository;
   let couponUserRepository: CouponUserRepository;
+  let userRepository: UserRepository;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
-      imports: [TypeOrmModule.forRoot(testConnectionOptions), CouponModule],
+      imports: [
+        TypeOrmModule.forRoot(testConnectionOptions),
+        CouponModule,
+        UserModule,
+      ],
     }).compile();
 
     service = module.get<CouponService>(CouponService);
     repository = module.get<CouponRepository>(CouponRepository);
-    couponUserRepository = module.get<CouponUserRepository>(CouponUserRepository);
+    couponUserRepository =
+      module.get<CouponUserRepository>(CouponUserRepository);
+    userRepository = module.get<UserRepository>(UserRepository);
   });
 
   beforeEach(async () => {
     await repository.clear();
+    await userRepository.clear();
   });
 
   afterAll(async () => {
@@ -55,6 +67,23 @@ describe('CouponService', () => {
       expect(result[0].type).toBe(CouponType.PRICE);
       expect(result[0].value).toBe(5_000);
       expect(result[0].stock).toBe(50);
+    });
+  });
+
+  describe('addSignUpCoupon', () => {
+    it('신규 회원에 대한 쿠폰을 추가할 수 있다.', async () => {
+      // given
+      const signUpCoupon = await repository.save(CouponFactory.signUpCoupon());
+      const user = await userRepository.save(UserFactory.of());
+
+      // when
+      await service.addSignUpCoupon(user.id);
+
+      // then
+      const result = await couponUserRepository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].couponId).toBe(signUpCoupon.id);
+      expect(result[0].userId).toBe(user.id);
     });
   });
 });
