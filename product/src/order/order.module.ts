e5afa367723f494c8e02ec7b602d650d02850d74
sjk5766 <wjdrnr6662@gmail.com>
2025-02-20 12:54:07 +0900

    feat: Order 모듈에 Product 모듈 의존성 추가

--- product/src/order/order.module.ts
+++ product/src/order/order.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ProductModule } from '../product/product.module';
 import { UserModule } from '../user/user.module';
 import { OrderController } from './order.controller';
 import { OrderRepository } from './order.repository';
 import { OrderService } from './order.service';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ProductModule],
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
 })
