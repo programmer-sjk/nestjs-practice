5c6a866a2f52315b6ea0fdc8fb0fc43e46dd9210
sjk5766 <wjdrnr6662@gmail.com>
2025-02-20 13:05:42 +0900

    feat: order item 레포지터리 추가

--- product/src/order/order.module.ts
+++ product/src/order/order.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProductModule } from '../product/product.module';
 import { UserModule } from '../user/user.module';
 import { OrderController } from './order.controller';
-import { OrderRepository } from './order.repository';
 import { OrderService } from './order.service';
+import { OrderItemRepository } from './repositories/order-item.repository';
+import { OrderRepository } from './repositories/order.repository';
 
 @Module({
   imports: [UserModule, ProductModule],
-  providers: [OrderService, OrderRepository],
+  providers: [OrderService, OrderRepository, OrderItemRepository],
   controllers: [OrderController],
 })
 export class OrderModule {}
