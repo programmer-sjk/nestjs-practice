c991c496db02b5b49c2169e4de7c275ed35577fe
sjk5766 <wjdrnr6662@gmail.com>
2025-02-20 13:01:03 +0900

    feat: id 배열로 product를 조회하는 함수 추가

--- product/src/product/product.service.ts
+++ product/src/product/product.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { In } from 'typeorm';
 import { ProductRegisterRequest } from './dto/product-register.request';
 import { ProductRepository } from './product.repository';
 
 @Injectable()
 export class ProductService {
   constructor(private readonly productRepository: ProductRepository) {}
- 
+
+  async findByIds(ids: number[]) {
+    return this.productRepository.findBy({ id: In(ids) });
+  }
+
   async addProduct(dto: ProductRegisterRequest) {
     await this.productRepository.save(dto.toEntity());
   }
