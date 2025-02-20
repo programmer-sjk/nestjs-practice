96afb925c89213a5c151a065a65dd3ae581bdf3c
sjk5766 <wjdrnr6662@gmail.com>
2025-02-20 12:48:29 +0900

    feat: Order 모듈에 User 모듈 의존성 추가

--- product/src/order/order.module.ts
+++ product/src/order/order.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from '../user/user.module';
 import { OrderController } from './order.controller';
 import { OrderRepository } from './order.repository';
 import { OrderService } from './order.service';
 
 @Module({
+  imports: [UserModule],
   providers: [OrderService, OrderRepository],
   controllers: [OrderController],
 })
