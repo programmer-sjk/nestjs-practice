e6695e8ef3119365247c11eaa6f4a9b10523478f
sjk5766 <wjdrnr6662@gmail.com>
2025-02-21 13:46:22 +0900

    feat: product 등록시 category가 존재하지 않으면 예외발생

--- product/src/product/product.service.ts
+++ product/src/product/product.service.ts
@@ -1,17 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { In } from 'typeorm';
+import { CategoryService } from '../category/category.service';
 import { ProductRegisterRequest } from './dto/product-register.request';
 import { ProductRepository } from './product.repository';
 
 @Injectable()
 export class ProductService {
-  constructor(private readonly productRepository: ProductRepository) {}
+  constructor(
+    private readonly categoryService: CategoryService,
+    private readonly productRepository: ProductRepository,
+  ) {}
 
   async findByIds(ids: number[]) {
     return this.productRepository.findBy({ id: In(ids) });
   }
 
   async addProduct(dto: ProductRegisterRequest) {
+    const category = await this.categoryService.findOneById(dto.categoryId);
+    if (!category) {
+      throw new BadRequestException('등록된 카테고리가 아닙니다.');
+    }
+
     await this.productRepository.save(dto.toEntity());
   }
 }
