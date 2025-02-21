478f520dbad56fe54586224c29ad40f1e03c5f66
sjk5766 <wjdrnr6662@gmail.com>
2025-02-21 12:57:49 +0900

    feat: 포인트 구매 기능에 User 권한 추가

--- product/src/point/point.controller.ts
+++ product/src/point/point.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 import { PointPurchaseRequest } from './dto/point-purchase.request';
 import { PointService } from './point.service';
 
@@ -6,6 +8,7 @@ import { PointService } from './point.service';
 export class PointController {
   constructor(private readonly pointService: PointService) {}
 
+  @Roles(Role.USER)
   @Post()
   async purchase(@Body() request: PointPurchaseRequest) {
     await this.pointService.addPointToUser(request.userId, request.value);
