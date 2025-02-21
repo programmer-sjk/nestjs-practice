ef2b029b1ce8602b8644092e37d06a774faf5514
sjk5766 <wjdrnr6662@gmail.com>
2025-02-21 14:07:39 +0900

    feat: 쿠폰 등록시 카테고리가 존재하지 않으면 예외처리

--- product/src/coupon/coupon.module.ts
+++ product/src/coupon/coupon.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { CategoryModule } from '../category/category.module';
 import { CouponController } from './coupon.controller';
 import { CouponService } from './coupon.service';
 import { CouponUserRepository } from './repositories/coupon-user.repository';
 import { CouponRepository } from './repositories/coupon.repository';
 
 @Module({
+  imports: [CategoryModule],
   providers: [CouponService, CouponRepository, CouponUserRepository],
   controllers: [CouponController],
   exports: [CouponService],
--- product/src/coupon/coupon.service.ts
+++ product/src/coupon/coupon.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CategoryService } from '../category/category.service';
+import { ERROR } from '../common/err-message';
 import { CouponRegisterRequest } from './dto/coupon-register.request';
 import { CouponUser } from './entities/coupon-user.entity';
 import { CouponUserRepository } from './repositories/coupon-user.repository';
@@ -9,12 +11,25 @@ export class CouponService {
   private readonly SIGN_UP_COUPON_NAME = '신규회원 쿠폰';
 
   constructor(
+    private readonly categoryService: CategoryService,
     private readonly couponRepository: CouponRepository,
     private readonly couponUserRepository: CouponUserRepository,
   ) {}
 
   async addCoupon(dto: CouponRegisterRequest) {
+    await this.validateCategory(dto.categoryId);
     await this.couponRepository.save(dto.toEntity());
+  }
+
+  private async validateCategory(categoryId?: number) {
+    if (!categoryId) {
+      return;
+    }
+
+    const category = await this.categoryService.findOneById(categoryId);
+    if (!category) {
+      throw new BadRequestException(ERROR.categoryNotFound);
+    }
   }
 
   async addSignUpCoupon(userId: number) {
