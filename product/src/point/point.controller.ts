a63391d4c3cba451381ac144c3b1db05ce75667c
sjk5766 <wjdrnr6662@gmail.com>
2025-02-21 12:57:25 +0900

    feat: 포인트 구매 기능

--- product/src/point/point.controller.ts
+++ product/src/point/point.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { PointPurchaseRequest } from './dto/point-purchase.request';
+import { PointService } from './point.service';
 
 @Controller('point')
-export class PointController {}
+export class PointController {
+  constructor(private readonly pointService: PointService) {}
+
+  @Post()
+  async purchase(@Body() request: PointPurchaseRequest) {
+    await this.pointService.addPointToUser(request.userId, request.value);
+  }
+}
