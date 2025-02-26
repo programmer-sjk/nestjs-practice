62f35dc56aeb1a6e594e83e577e2fac973a78bfa
sjk5766 <wjdrnr6662@gmail.com>
2025-02-26 15:26:07 +0900

    chore: order 모듈에 coupon, point 모듈 의존성 추가

--- product/src/order/order.module.ts
+++ product/src/order/order.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { CouponModule } from '../coupon/coupon.module';
+import { PointModule } from '../point/point.module';
 import { ProductModule } from '../product/product.module';
 import { UserModule } from '../user/user.module';
 import { OrderController } from './order.controller';
@@ -7,7 +9,7 @@ import { OrderItemRepository } from './repositories/order-item.repository';
 import { OrderRepository } from './repositories/order.repository';
 
 @Module({
-  imports: [UserModule, ProductModule],
+  imports: [UserModule, ProductModule, PointModule, CouponModule],
   providers: [OrderService, OrderRepository, OrderItemRepository],
   controllers: [OrderController],
 })
