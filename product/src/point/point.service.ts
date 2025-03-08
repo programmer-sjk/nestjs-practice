efe637eecf04e3c3e9aa554cb1782b1374b0c8a5
sjk5766 <wjdrnr6662@gmail.com>
2025-03-08 11:29:30 +0900

    feat: 포인트 적립/사용시 detail 테이블에도 저장하도록 수정

--- product/src/point/point.service.ts
+++ product/src/point/point.service.ts
@@ -1,12 +1,15 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { DateTime } from 'luxon';
 import { IsNull, MoreThan, Or } from 'typeorm';
+import { Transactional } from 'typeorm-transactional';
 import { DateUtil } from '../common/date-util';
 import { OrderBy } from '../common/enums/order-by.enum';
 import { ERROR } from '../common/err-message';
 import { PointHistoryResponse } from './dto/point-history.response';
+import { PointHistoryDetail } from './entities/point-history-detail.entity';
 import { PointHistory } from './entities/point-history.entity';
 import { PointType } from './enums/point-type.enum';
+import { PointHistoryDetailRepository } from './repositories/point-history-detail.repository';
 import { PointHistoryRepository } from './repositories/point-history.repository';
 
 @Injectable()
@@ -15,6 +18,7 @@ export class PointService {
 
   constructor(
     private readonly pointHistoryRepository: PointHistoryRepository,
+    private readonly historyDetailRepository: PointHistoryDetailRepository,
   ) {}
 
   async findHistory(userId: number) {
@@ -48,15 +52,21 @@ export class PointService {
     return points.reduce((acc, cur) => acc + cur.value, 0);
   }
 
+  @Transactional()
   async addPointByPurchase(userId: number, value: number) {
-    await this.pointHistoryRepository.save(
+    const point = await this.pointHistoryRepository.save(
       PointHistory.earn(userId, value, PointType.PURCHASE),
+    );
+
+    await this.historyDetailRepository.save(
+      PointHistoryDetail.earn(userId, value, point.id),
     );
   }
 
+  @Transactional()
   async addSignUpPointToUser(userId: number) {
     const expiredAt = DateTime.now().plus({ months: 3 });
-    await this.pointHistoryRepository.save(
+    const point = await this.pointHistoryRepository.save(
       PointHistory.earn(
         userId,
         this.SIGN_UP_POINT,
@@ -64,18 +74,27 @@ export class PointService {
         expiredAt,
       ),
     );
+
+    await this.historyDetailRepository.save(
+      PointHistoryDetail.earn(userId, this.SIGN_UP_POINT, point.id),
+    );
   }
 
+  @Transactional()
   async usePoint(userId: number, value: number, type: PointType) {
-    const point = await this.getUserTotalPoint(userId);
-    const totalPoint = point - value;
+    const userPoint = await this.getUserTotalPoint(userId);
+    const totalPoint = userPoint - value;
 
     if (totalPoint < 0) {
       throw new BadRequestException(ERROR.pointNotEnough);
     }
 
-    await this.pointHistoryRepository.save(
+    const point = await this.pointHistoryRepository.save(
       PointHistory.use(userId, -value, type),
+    );
+
+    await this.historyDetailRepository.save(
+      PointHistoryDetail.use(userId, -value, point.id),
     );
   }
 }
