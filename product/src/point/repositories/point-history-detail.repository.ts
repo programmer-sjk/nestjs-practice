45385bee410b1cf3dbc6cc2a2ae541c0f0a90bac
sjk5766 <wjdrnr6662@gmail.com>
2025-03-10 13:16:36 +0900

    feat: pointHistoryId로 group by해서 value의 합이 0보다 큰 id 배열을 조회하는 함수 추가

--- product/src/point/repositories/point-history-detail.repository.ts
+++ product/src/point/repositories/point-history-detail.repository.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
+import { OrderBy } from '../../common/enums/order-by.enum';
 import { PointHistoryDetail } from '../entities/point-history-detail.entity';
 
 @Injectable()
 export class PointHistoryDetailRepository extends Repository<PointHistoryDetail> {
   constructor(private readonly dataSource: DataSource) {
     super(PointHistoryDetail, dataSource.createEntityManager());
+  }
+
+  async getAvaiableUserPointIds(userId: number) {
+    return this.createQueryBuilder('detail')
+      .select('detail.id')
+      .where('detail.userId = :userId', { userId })
+      .orderBy('detail.id', OrderBy.ASC)
+      .groupBy('detail.pointHistoryId')
+      .having('SUM(detail.value) > 0')
+      .getRawMany();
   }
 }
