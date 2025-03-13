ec6a02370526a2b750c02b9fb794a4f774316628
sjk5766 <wjdrnr6662@gmail.com>
2025-03-13 14:03:58 +0900

    test: couponService에 대한 테스트 케이스 추가

--- product/test/coupon/coupon.service.spec.ts
+++ product/test/coupon/coupon.service.spec.ts
@@ -1,8 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import {
+  initializeTransactionalContext,
+  StorageDriver,
+} from 'typeorm-transactional';
 import { CouponModule } from '../../src/coupon/coupon.module';
 import { CouponService } from '../../src/coupon/coupon.service';
 import { CouponRegisterRequest } from '../../src/coupon/dto/coupon-register.request';
+import { CouponUser } from '../../src/coupon/entities/coupon-user.entity';
 import { CouponType } from '../../src/coupon/enums/coupon-type.enum';
 import { CouponUserRepository } from '../../src/coupon/repositories/coupon-user.repository';
 import { CouponRepository } from '../../src/coupon/repositories/coupon.repository';
@@ -10,7 +15,7 @@ import { UserModule } from '../../src/user/user.module';
 import { UserRepository } from '../../src/user/user.repository';
 import { CouponFactory } from '../fixture/entities/coupon-factory';
 import { UserFactory } from '../fixture/entities/user-factory';
-import { testConnectionOptions } from '../test-ormconfig';
+import { testTypeormOptions } from '../test-ormconfig';
 
 describe('CouponService', () => {
   let module: TestingModule;
@@ -20,9 +25,10 @@ describe('CouponService', () => {
   let userRepository: UserRepository;
 
   beforeAll(async () => {
+    initializeTransactionalContext({ storageDriver: StorageDriver.AUTO });
     module = await Test.createTestingModule({
       imports: [
-        TypeOrmModule.forRoot(testConnectionOptions),
+        TypeOrmModule.forRootAsync(testTypeormOptions),
         CouponModule,
         UserModule,
       ],
@@ -38,6 +44,7 @@ describe('CouponService', () => {
   beforeEach(async () => {
     await repository.clear();
     await userRepository.clear();
+    await couponUserRepository.clear();
   });
 
   afterAll(async () => {
@@ -46,6 +53,38 @@ describe('CouponService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('giveCouponToUser', () => {
+    it('쿠폰을 사용자에게 발급할 수 있다.', async () => {
+      // given
+      const coupon = await repository.save(CouponFactory.signUpCoupon());
+      const user = await userRepository.save(UserFactory.of());
+
+      // when
+      await service.giveCouponToUser(coupon.id, user.id);
+
+      // then
+      const result = await couponUserRepository.find();
+      expect(result[0].couponId).toBe(coupon.id);
+      expect(result[0].userId).toBe(user.id);
+    });
+  });
+
+  describe('findUserCoupon', () => {
+    it('사용자의 쿠폰을 조회할 수 있다.', async () => {
+      // given
+      const coupon = await repository.save(CouponFactory.signUpCoupon());
+      const user = await userRepository.save(UserFactory.of());
+
+      // when
+      const result = await service.findUserCoupon(coupon.id, user.id);
+
+      // then
+      const couponUser = result.couponUsers[0];
+      expect(couponUser.couponId).toBe(coupon.id);
+      expect(couponUser.userId).toBe(user.id);
+    });
   });
 
   describe('addCoupon', () => {
@@ -84,6 +123,40 @@ describe('CouponService', () => {
       expect(result).toHaveLength(1);
       expect(result[0].couponId).toBe(signUpCoupon.id);
       expect(result[0].userId).toBe(user.id);
+    });
+  });
+
+  describe('useCoupon', () => {
+    it('사용자가 쿠폰을 사용할 수 있다.', async () => {
+      // given
+      const signUpCoupon = await repository.save(CouponFactory.signUpCoupon());
+      const user = await userRepository.save(UserFactory.of());
+      await couponUserRepository.save(CouponUser.of(signUpCoupon.id, user.id));
+
+      // when
+      await service.useCoupon(signUpCoupon.id, user.id);
+
+      // then
+      const result = await couponUserRepository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].isUsed).toBe(true);
+    });
+  });
+
+  describe('refundCoupon', () => {
+    it('사용한 쿠폰을 취소할 수 있다.', async () => {
+      // given
+      const signUpCoupon = await repository.save(CouponFactory.signUpCoupon());
+      const user = await userRepository.save(UserFactory.of());
+      await couponUserRepository.save(CouponUser.of(signUpCoupon.id, user.id));
+
+      // when
+      await service.refundCoupon(signUpCoupon.id, user.id);
+
+      // then
+      const result = await couponUserRepository.find();
+      expect(result).toHaveLength(1);
+      expect(result[0].isUsed).toBe(false);
     });
   });
 });
--- product/test/fixture/entities/coupon-user-factory.ts
+++ product/test/fixture/entities/coupon-user-factory.ts
@@ -3,7 +3,13 @@ import { CouponUser } from '../../../src/coupon/entities/coupon-user.entity';
 export class CouponUserFactory {
   private constructor() {}
 
-  static create(couponId: number, userId: number) {
+  static of(couponId: number, userId: number) {
     return CouponUser.of(couponId, userId);
+  }
+
+  static createUsed(couponId: number, userId: number) {
+    const couponUser = this.of(couponId, userId);
+    couponUser.isUsed = true;
+    return couponUser;
   }
 }
