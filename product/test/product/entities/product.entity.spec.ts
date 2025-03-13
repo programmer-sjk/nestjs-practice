3ea1816419794fa67cd7a620de4694aeacc52d00
sjk5766 <wjdrnr6662@gmail.com>
2025-03-13 14:39:48 +0900

    test: product 엔티티 테스트 추가

---
+++ product/test/product/entities/product.entity.spec.ts
@@ -0,0 +1,29 @@
+import { BadRequestException } from '@nestjs/common';
+import { Product } from '../../../src/product/entities/product.entity';
+
+describe('Product', () => {
+  describe('use', () => {
+    it('상품의 재고를 감소한다.', () => {
+      // given
+      const product = new Product();
+      product.stock = 10;
+
+      // when
+      product.decreaseStock();
+
+      // then
+      expect(product.stock).toBe(9);
+    });
+
+    it('상품의 재고가 없는데 감소하면 예외가 발생한다.', () => {
+      // given
+      const product = new Product();
+      product.stock = 0;
+
+      // when & then
+      expect(() => product.decreaseStock()).toThrowError(
+        new BadRequestException('재고가 없습니다.'),
+      );
+    });
+  });
+});
