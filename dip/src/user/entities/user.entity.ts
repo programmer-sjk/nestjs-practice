5417c0b36ec79af6b38172b8b32dfb760b4d442d
sjk5766 <wjdrnr6662@gmail.com>
2025-03-19 14:17:01 +0900

    feat: user 엔티티 추가

---
+++ dip/src/user/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Index('email', { unique: true })
+  @Column({ length: 16 })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  static of(email: string, password: string) {
+    const user = new User();
+    user.email = email;
+    user.password = password;
+    return user;
+  }
+}
