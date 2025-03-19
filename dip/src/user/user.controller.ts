30ca5138289511762f90933b901ced199ab2d024
sjk5766 <wjdrnr6662@gmail.com>
2025-03-19 14:39:38 +0900

    feat: 전체 사용자 조회하는 기능

--- dip/src/user/user.controller.ts
+++ dip/src/user/user.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common';
+import { UserService } from './user.service';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async findAll() {
+    return this.userService.findAll();
+  }
+
+  @Get('/:id')
+  async find() {}
+
+  @Post()
+  async signUp() {}
+
+  @Patch()
+  async updatePassword() {}
+
+  @Delete()
+  async withdraw() {}
+}
