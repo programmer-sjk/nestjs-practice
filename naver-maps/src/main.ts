19e530bbc0a0770a9e7423e5e2d07689649ae64e
sjk5766 <wjdrnr6662@gmail.com>
2025-04-08 13:02:48 +0900

    feat: 전역 설정을 main.ts에 적요

--- naver-maps/src/main.ts
+++ naver-maps/src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setNestApp } from './maps/common/set-nest-app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('v1');
+  setNestApp(app);
   await app.listen(3000);
 }
 bootstrap();
