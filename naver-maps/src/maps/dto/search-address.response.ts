63bfcab910570c7b07ae4a54df9365008be739b0
sjk5766 <wjdrnr6662@gmail.com>
2025-04-08 13:04:14 +0900

    feat: 주소 검색 응답 dto 추가

---
+++ naver-maps/src/maps/dto/search-address.response.ts
@@ -0,0 +1,24 @@
+import { Exclude, Expose } from 'class-transformer';
+
+export class SearchAddressResponse {
+  @Exclude()
+  private readonly _roadAddress: string;
+
+  @Exclude()
+  private readonly _jibunAddress: string;
+
+  constructor(roadAddress: string, jibunAddress: string) {
+    this._roadAddress = roadAddress;
+    this._jibunAddress = jibunAddress;
+  }
+
+  @Expose()
+  get roadAddress() {
+    return this._roadAddress;
+  }
+
+  @Expose()
+  get jibunAddress() {
+    return this._jibunAddress;
+  }
+}
