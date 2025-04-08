524fc492ebd50ea2652b37ab4b3476c0ccafcb4f
sjk5766 <wjdrnr6662@gmail.com>
2025-04-08 13:09:46 +0900

    feat: application 공통 설정 중 validation 설정 제거

--- naver-maps/src/maps/common/set-nest-app.ts
+++ naver-maps/src/maps/common/set-nest-app.ts
@@ -1,12 +1,7 @@
-import {
-  ClassSerializerInterceptor,
-  INestApplication,
-  ValidationPipe
-} from '@nestjs/common';
+import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 export function setNestApp(app: INestApplication) {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('v1');
 }
