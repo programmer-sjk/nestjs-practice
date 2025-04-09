7bbceb6eeb02f91c8b512ea8891bf7575d499939
sjk5766 <wjdrnr6662@gmail.com>
2025-04-09 14:16:05 +0900

    feat: 네이버 지도 API에 ApiOperation을 이용한 설명 추가

--- naver-maps/src/maps/maps.controller.ts
+++ naver-maps/src/maps/maps.controller.ts
@@ -7,6 +7,7 @@ import {
   Res,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { Response } from 'express';
 import { MapsService } from './maps.service';
 
@@ -14,12 +15,14 @@ import { MapsService } from './maps.service';
 export class MapsController {
   constructor(private readonly mapsService: MapsService) {}
 
+  @ApiOperation({ description: '정적 지도 이미지 반환' })
   @Get()
   async getStaticMaps(@Res() res: Response) {
     const result = await this.mapsService.getStaticMaps();
     return res.contentType('image/jpeg').send(result);
   }
 
+  @ApiOperation({ description: '줌이 가능한 동적 html 반환' })
   @Get('dynamic')
   @Render('dynamic-map')
   async getDynamicMaps(@Query('addr') address: string) {
@@ -32,11 +35,13 @@ export class MapsController {
   }
 
   @UseInterceptors(ClassSerializerInterceptor)
+  @ApiOperation({ description: '검색한 주소에 대한 도로명/지번 주소 반환' })
   @Get('address')
   async getAddress(@Query('q') address: string) {
     return this.mapsService.getAddress(address);
   }
 
+  @ApiOperation({ description: '검색한 주소에 대한 정적 이미지 반환' })
   @Get('search')
   async search(@Query('addr') address: string, @Res() res: Response) {
     const result = await this.mapsService.searchMaps(address);
