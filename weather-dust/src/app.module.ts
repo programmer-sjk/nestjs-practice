a1f3d27b11aedfa0b42497e6952c5a90b337b1b4
sjk5766 <wjdrnr6662@gmail.com>
2025-04-09 15:38:38 +0900

    feat: app 모듈에 config 모듈을 전역으로 등록

--- weather-dust/src/app.module.ts
+++ weather-dust/src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { DustModule } from './dust/dust.module';
 
 @Module({
-  imports: [DustModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DustModule,
+  ],
 })
 export class AppModule {}
