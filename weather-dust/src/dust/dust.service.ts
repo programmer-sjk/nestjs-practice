f862cb673d9852d8f454d0f8293a194c2ead19f9
sjk5766 <wjdrnr6662@gmail.com>
2025-04-09 15:42:42 +0900

    feat: config 서비스를 활용해 미세먼지 요청 url 만드는 코드 추가

--- weather-dust/src/dust/dust.service.ts
+++ weather-dust/src/dust/dust.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class DustService {}
+export class DustService {
+  private readonly startIdx = 1;
+  private readonly endIdx = 25;
+
+  private readonly authKey;
+  private readonly dustUrl;
+
+  constructor(private readonly configService: ConfigService) {
+    this.authKey = this.configService.get('DUST_AUTH_KEY');
+    this.dustUrl = `http://openapi.seoul.go.kr:8088/${this.authKey}/json/ListAirQualityByDistrictService/${this.startIdx}/${this.endIdx}`;
+  }
+}
