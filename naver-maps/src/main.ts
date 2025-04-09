22e88d67d68829dfde336ffae878299665dd74e7
sjk5766 <wjdrnr6662@gmail.com>
2025-04-09 14:17:47 +0900

    refactor: main에 설정이 많아져서 함수로 구분

--- naver-maps/src/main.ts
+++ naver-maps/src/main.ts
@@ -8,17 +8,24 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.setGlobalPrefix('v1');
+  setViewEngine(app);
+  setSwaggerConfig(app);
+
+  await app.listen(3000);
+}
+bootstrap();
+
+function setViewEngine(app: NestExpressApplication) {
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('hbs');
+}
 
+function setSwaggerConfig(app: NestExpressApplication) {
   const config = new DocumentBuilder()
     .setTitle('Api Document')
     .setVersion('1.0')
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
-
-  await app.listen(3000);
 }
-bootstrap();
