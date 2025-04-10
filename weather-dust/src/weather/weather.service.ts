ba6f7e93ae25ccfe7a5ecdc640ba40fc0b16462e
sjk5766 <wjdrnr6662@gmail.com>
2025-04-10 13:32:44 +0900

    feat: config 서비스를 활용해 날씨 요청을 만드는 url 추가

--- weather-dust/src/weather/weather.service.ts
+++ weather-dust/src/weather/weather.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class WeatherService {}
+export class WeatherService {
+  private readonly weatherUrl;
+
+  constructor(private readonly configService: ConfigService) {
+    const authKey = this.configService.get('WEATHER_AUTH_KEY');
+    this.weatherUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=${authKey}`
+  }
+}
