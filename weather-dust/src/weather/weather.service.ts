cd234907d5eabf196ba2791c496087600e68f2df
sjk5766 <wjdrnr6662@gmail.com>
2025-04-10 13:48:08 +0900

    feat: 날씨 응답에 dto 반영

--- weather-dust/src/weather/weather.service.ts
+++ weather-dust/src/weather/weather.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import axios from 'axios';
+import { WeatherResponse } from './dto/weather-response';
 
 @Injectable()
 export class WeatherService {
@@ -13,6 +14,11 @@ export class WeatherService {
 
   async getWeather(city: string) {
     const response = await axios.get(`${this.weatherUrl}&q=${city}`);
-    return response.data;
+    const weather = response.data;
+    return new WeatherResponse(
+      weather.weather[0].main,
+      weather.main.temp,
+      weather.main.feels_like,
+    );
   }
 }
