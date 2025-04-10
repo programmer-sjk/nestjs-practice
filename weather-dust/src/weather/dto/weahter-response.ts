75b4fa943d222ec8f7e74425a3a00b769f42e43c
sjk5766 <wjdrnr6662@gmail.com>
2025-04-10 13:45:39 +0900

    feat: 날씨 응답 dto 추가

---
+++ weather-dust/src/weather/dto/weahter-response.ts
@@ -0,0 +1,28 @@
+import { Exclude, Expose } from 'class-transformer';
+
+export class DustResponse {
+  @Exclude() private readonly _summary: string;
+  @Exclude() private readonly _temp: number;
+  @Exclude() private readonly _feelTemp: number;
+
+  constructor(summary: string, temp: number, feelTemp: number) {
+    this._summary = summary;
+    this._temp = temp;
+    this._feelTemp = feelTemp;
+  }
+
+  @Expose()
+  get summary(): string {
+    return this._summary;
+  }
+
+  @Expose()
+  get temp(): number {
+    return this._temp;
+  }
+
+  @Expose()
+  get feelTemp(): number {
+    return Number(this._feelTemp);
+  }
+}
