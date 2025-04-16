8fcb3eb364b0614aaa1e0781cb9c5a1a1b7eb40b
sjk5766 <wjdrnr6662@gmail.com>
2025-04-16 14:39:46 +0900

    feat: 게시글 등록 기능 추가

--- board/src/board/board.controller.ts
+++ board/src/board/board.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ResponseEntity } from '../common/response-entity';
+import { BoardService } from './board.service';
+import { AddBoardRequest } from './dto/add-board.request';
 
 @Controller('board')
-export class BoardController {}
+export class BoardController {
+  constructor(private readonly boardService: BoardService) {}
+
+  @Post()
+  async register(@Body() request: AddBoardRequest) {
+    await this.boardService.add(request)
+    return ResponseEntity.OK()
+  }
+}
--- board/src/board/board.service.ts
+++ board/src/board/board.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { BoardRepository } from './board.repository';
+import { AddBoardRequest } from './dto/add-board.request';
 
 @Injectable()
-export class BoardService {}
+export class BoardService {
+  constructor(private readonly boardRepository: BoardRepository) {}
+
+  async add(dto: AddBoardRequest) {
+    return this.boardRepository.save(dto.toEntity())
+  }
+}
