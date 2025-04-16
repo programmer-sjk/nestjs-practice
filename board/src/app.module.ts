cdfede64e7111a1f7c07e111883ff7f797c5af58
sjk5766 <wjdrnr6662@gmail.com>
2025-04-16 14:12:14 +0900

    feat: user 모듈, 컨트롤러, 서비스 추가

--- board/src/app.module.ts
+++ board/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
 import { BoardModule } from './board/board.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule, UserModule],
 })
 export class AppModule {}
