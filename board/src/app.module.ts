bf271cbc4063a78bb86837d58b15875e4805089c
sjk5766 <wjdrnr6662@gmail.com>
2025-04-16 14:09:23 +0900

    feat: app 모듈에 typeorm 설정 추가

--- board/src/app.module.ts
+++ board/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../ormconfig';
 import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [BoardModule]
+  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule],
 })
 export class AppModule {}
