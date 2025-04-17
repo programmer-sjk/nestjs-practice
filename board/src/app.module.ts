d1c8d1c2c13fd3eccf120da8a5f19c19bda483f9
sjk5766 <wjdrnr6662@gmail.com>
2025-04-17 14:45:35 +0900

    feat: comment 모듈, 컨트롤러, 서비스 추가

--- board/src/app.module.ts
+++ board/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
 import { BoardModule } from './board/board.module';
 import { UserModule } from './user/user.module';
+import { CommentModule } from './comment/comment.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule, UserModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), BoardModule, UserModule, CommentModule],
 })
 export class AppModule {}
