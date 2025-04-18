af719d3c7a361acf2da1300659bc62aa98001777
sjk5766 <wjdrnr6662@gmail.com>
2025-04-18 13:11:46 +0900

    feat: user 서비스 exports 설정에 추가

--- board/src/user/user.module.ts
+++ board/src/user/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
+import { UserService } from './user.service';
 
 @Module({
   providers: [UserService, UserRepository],
-  controllers: [UserController]
+  controllers: [UserController],
+  exports: [UserService],
 })
 export class UserModule {}
