90a69fc381bdb5b205688ce2f17451a7becb8bf0
sjk5766 <wjdrnr6662@gmail.com>
2025-04-18 13:13:21 +0900

    feat: comment 레포지터리 추가

--- board/src/comment/comment.module.ts
+++ board/src/comment/comment.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { CommentService } from './comment.service';
 import { CommentController } from './comment.controller';
+import { CommentRepository } from './comment.repository';
+import { CommentService } from './comment.service';
 
 @Module({
-  providers: [CommentService],
-  controllers: [CommentController]
+  providers: [CommentService, CommentRepository],
+  controllers: [CommentController],
 })
 export class CommentModule {}
