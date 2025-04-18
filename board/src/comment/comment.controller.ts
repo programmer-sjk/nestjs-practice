56f478f30e9e9b8bc1b212d804fadb14c6f63393
sjk5766 <wjdrnr6662@gmail.com>
2025-04-18 14:16:16 +0900

    feat: 댓글 추가, 수정, 삭제시 로그인이 필요하도록 데코레이터 추가

--- board/src/comment/comment.controller.ts
+++ board/src/comment/comment.controller.ts
@@ -1,4 +1,6 @@
 import { Body, Controller, Delete, Patch, Post } from '@nestjs/common';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { Role } from '../auth/enums/role.enum';
 import { ResponseEntity } from '../common/response-entity';
 import { CommentService } from './comment.service';
 import { AddCommentRequest } from './dto/add-comment.request';
@@ -9,21 +11,24 @@ import { UpdateCommentRequest } from './dto/update-comment.request';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
+  @Roles(Role.USER)
   @Post()
   async register(@Body() request: AddCommentRequest) {
     await this.commentService.register(request);
-    return ResponseEntity.OK()
+    return ResponseEntity.OK();
   }
 
+  @Roles(Role.USER)
   @Patch()
   async update(@Body() request: UpdateCommentRequest) {
     await this.commentService.update(request);
-    return ResponseEntity.OK()
+    return ResponseEntity.OK();
   }
 
+  @Roles(Role.USER)
   @Delete()
   async remove(@Body() request: RemoveCommentRequest) {
     await this.commentService.remove(request);
-    return ResponseEntity.OK()
+    return ResponseEntity.OK();
   }
 }
