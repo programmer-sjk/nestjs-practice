b9f9c450b5fa2b8285599c977233fbb51a9431c2
sjk5766 <wjdrnr6662@gmail.com>
2025-04-18 15:13:31 +0900

    feat: app 모듈에 전역 auth guard 설정 추가

--- board/src/app.module.ts
+++ board/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { CommentModule } from './comment/comment.module';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -13,6 +16,13 @@ import { AuthModule } from './auth/auth.module';
     UserModule,
     CommentModule,
     AuthModule,
+    JwtModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule {}
