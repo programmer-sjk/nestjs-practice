3693a20a71406b05ee778b4739a04abd395d41ac
sjk5766 <wjdrnr6662@gmail.com>
2025-04-20 14:57:20 +0900

    feat: redis 모듈에 설정 추가

--- board/src/redis/redis.module.ts
+++ board/src/redis/redis.module.ts
@@ -1,9 +1,16 @@
+import { RedisModule as NestRedisModule } from '@nestjs-modules/ioredis';
 import { Module } from '@nestjs/common';
-import { RedisService } from './redis.service';
 import { RedisController } from './redis.controller';
+import { RedisService } from './redis.service';
 
 @Module({
+  imports: [
+    NestRedisModule.forRoot({
+      type: 'single',
+      url: 'redis://localhost:6379',
+    }),
+  ],
   providers: [RedisService],
-  controllers: [RedisController]
+  controllers: [RedisController],
 })
 export class RedisModule {}
--- board/src/redis/redis.service.ts
+++ board/src/redis/redis.service.ts
@@ -1,4 +1,8 @@
+import { InjectRedis } from '@nestjs-modules/ioredis';
 import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
-export class RedisService {}
+export class RedisService {
+  constructor(@InjectRedis() private readonly redis: Redis) {}
+}
