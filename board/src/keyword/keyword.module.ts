4c15f3588b980f48997e495b1dd41c4afc8c0974
sjk5766 <wjdrnr6662@gmail.com>
2025-04-21 14:38:38 +0900

    feat: keyword 모듈에서 redis 모듈 import

--- board/src/keyword/keyword.module.ts
+++ board/src/keyword/keyword.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { KeywordService } from './keyword.service';
+import { RedisModule } from '../redis/redis.module';
 import { KeywordController } from './keyword.controller';
+import { KeywordService } from './keyword.service';
 
 @Module({
+  imports: [RedisModule],
   providers: [KeywordService],
-  controllers: [KeywordController]
+  controllers: [KeywordController],
 })
 export class KeywordModule {}
