4b8e8a677d63e0f13691bcd5957a40efdd7b49ba
sjk5766 <wjdrnr6662@gmail.com>
2025-04-21 14:38:54 +0900

    feat: 게시글 인기 검색어를 제공하는 API

--- board/src/keyword/keyword.controller.ts
+++ board/src/keyword/keyword.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ResponseEntity } from '../common/response-entity';
+import { KeywordService } from './keyword.service';
 
 @Controller('keyword')
-export class KeywordController {}
+export class KeywordController {
+  constructor(private readonly keywordService: KeywordService) {}
+
+  @Get('post')
+  async postPopularKeyword() {
+    const keywords = await this.keywordService.getPostPopularKeywords();
+    return ResponseEntity.OK(keywords)
+  }
+}
--- board/src/keyword/keyword.service.ts
+++ board/src/keyword/keyword.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { RedisService } from '../redis/redis.service';
 
 @Injectable()
-export class KeywordService {}
+export class KeywordService {
+  private readonly keywordsCount = 5;
+  private readonly postKeywordKey = 'sorted-set:post:popular-keyword';
+
+  constructor(private readonly redisService: RedisService) {}
+
+  async getPostPopularKeywords() {
+    return this.redisService.zrevrange(this.postKeywordKey, this.keywordsCount);
+  }
+}
