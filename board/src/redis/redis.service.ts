3fa9dba8cb6a400683ea6c0d06fdee06b53501b6
sjk5766 <wjdrnr6662@gmail.com>
2025-04-21 14:36:03 +0900

    feat: redis 서비스에 zincrby 메서드 추가

--- board/src/redis/redis.service.ts
+++ board/src/redis/redis.service.ts
@@ -4,5 +4,11 @@ import Redis from 'ioredis';
 
 @Injectable()
 export class RedisService {
+  private readonly incrAmount = 1;
+
   constructor(@InjectRedis() private readonly redis: Redis) {}
+
+  async zincrby(key: string, member: string) {
+    return this.redis.zincrby(key, this.incrAmount, member);
+  }
 }
