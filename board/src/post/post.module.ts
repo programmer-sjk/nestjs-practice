5f6593408c455fc879e9f3fd8791ede10f697440
sjk5766 <wjdrnr6662@gmail.com>
2025-04-21 14:36:55 +0900

    feat: post 모듈에서 redis 모듈 imports

--- board/src/post/post.module.ts
+++ board/src/post/post.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentModule } from '../comment/comment.module';
+import { RedisModule } from '../redis/redis.module';
 import { PostController } from './post.controller';
 import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [CommentModule],
+  imports: [CommentModule, RedisModule],
   providers: [PostService, PostRepository],
   controllers: [PostController],
 })
