96d28af56dea7b02f5c9857b51e29269537672e9
sjk5766 <wjdrnr6662@gmail.com>
2025-04-25 13:01:37 +0900

    feat: post 모듈에서 EventEmitterModule 모듈 imports

--- board/src/post/post.module.ts
+++ board/src/post/post.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
 import { CommentModule } from '../comment/comment.module';
 import { RedisModule } from '../redis/redis.module';
 import { PostController } from './post.controller';
@@ -6,7 +7,7 @@ import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [CommentModule, RedisModule],
+  imports: [CommentModule, RedisModule, EventEmitterModule.forRoot()],
   providers: [PostService, PostRepository],
   controllers: [PostController],
 })
