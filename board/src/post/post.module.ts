059b0e281ca0bdf7539638cea7e005c547b2cff9
sjk5766 <wjdrnr6662@gmail.com>
2025-04-25 13:42:21 +0900

    feat: post 모듈에 post 리스너를 프로바이더로 추가

--- board/src/post/post.module.ts
+++ board/src/post/post.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
 import { CommentModule } from '../comment/comment.module';
 import { RedisModule } from '../redis/redis.module';
+import { PostListener } from './listeners/search-listener';
 import { PostController } from './post.controller';
 import { PostRepository } from './post.repository';
 import { PostService } from './post.service';
 
 @Module({
   imports: [CommentModule, RedisModule, EventEmitterModule.forRoot()],
-  providers: [PostService, PostRepository],
+  providers: [PostService, PostRepository, PostListener],
   controllers: [PostController],
 })
 export class PostModule {}
