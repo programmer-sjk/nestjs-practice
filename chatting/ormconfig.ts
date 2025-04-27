51c72d1b9fab4a8e4651996e6ebde9ed353381af
sjk5766 <wjdrnr6662@gmail.com>
2025-04-27 16:11:11 +0900

    chore: ormconfig.ts 파일 추가

---
+++ chatting/ormconfig.ts
@@ -0,0 +1,17 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+const connectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'chatting',
+  database: 'calendar',
+  logging: true,
+  synchronize: false,
+  entities: [__dirname + '/src/**/entities/*.{js,ts}'],
+  migrations: [__dirname + '/src/migrations/*.{js,ts}'],
+  migrationsTableName: 'migrations',
+};
+
+const dataSource = new DataSource(connectionOptions);
+export default dataSource;
