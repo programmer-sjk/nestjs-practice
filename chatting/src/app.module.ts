cc4e6912c7b2fa88ca500e97fd1ab06b7840893c
sjk5766 <wjdrnr6662@gmail.com>
2025-04-28 13:11:55 +0900

    feat: group 모듈, 컨트롤러, 서비스 추가

--- chatting/src/app.module.ts
+++ chatting/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
 import { UserModule } from './user/user.module';
+import { GroupModule } from './group/group.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, GroupModule],
 })
 export class AppModule {}
