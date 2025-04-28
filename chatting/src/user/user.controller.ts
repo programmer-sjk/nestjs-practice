5fe03b9bfd9e675ccbffdfc7820d36bee28fb77c
sjk5766 <wjdrnr6662@gmail.com>
2025-04-28 13:31:51 +0900

    feat: 사용자 회원가입 기능

--- chatting/src/user/user.controller.ts
+++ chatting/src/user/user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
+import { ResponseEntity } from '../common/response-entity';
+import { SignUpRequest } from './dto/sign-up.request';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async signUp(request: SignUpRequest) {
+    await this.userService.signUp(request);
+    return ResponseEntity.OK();
+  }
+}
