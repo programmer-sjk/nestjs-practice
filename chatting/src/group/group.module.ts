a22e468fc7d696120c737487a2770556d0af92a4
sjk5766 <wjdrnr6662@gmail.com>
2025-04-28 15:01:50 +0900

    feat: Group User 레포지터리 추가

--- chatting/src/group/group.module.ts
+++ chatting/src/group/group.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GroupController } from './group.controller';
-import { GroupRepository } from './repositories/group.repository';
 import { GroupService } from './group.service';
+import { GroupUserRepository } from './repositories/group-user.repository';
+import { GroupRepository } from './repositories/group.repository';
 
 @Module({
-  providers: [GroupService, GroupRepository],
+  providers: [GroupService, GroupRepository, GroupUserRepository],
   controllers: [GroupController],
 })
 export class GroupModule {}
