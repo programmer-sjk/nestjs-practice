b071b970d2441895ee4287a906c9bbec9443c83c
sjk5766 <wjdrnr6662@gmail.com>
2025-04-29 15:50:31 +0900

    feat: 그룹 초대자가 그룹에 없으면 예외처리 하는 비공개 메서드 추가

--- chatting/src/group/group.service.ts
+++ chatting/src/group/group.service.ts
@@ -1,12 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { AddGroupRequest } from './dto/add-group.request';
+import { GroupUserRepository } from './repositories/group-user.repository';
 import { GroupRepository } from './repositories/group.repository';
 
 @Injectable()
 export class GroupService {
-  constructor(private readonly groupRepository: GroupRepository) {}
+  constructor(
+    private readonly groupRepository: GroupRepository,
+    private readonly groupUserRepository: GroupUserRepository,
+  ) {}
 
   async addGroup(dto: AddGroupRequest) {
     await this.groupRepository.save(dto.toEntity());
+  }
+
+  private async validateInvite(groupId: number, invitorId: number) {
+    const userInGroup = await this.groupUserRepository.findOneBy({
+      groupId,
+      userId: invitorId,
+    });
+    if (!userInGroup) {
+      throw new BadRequestException('사용자가 채팅 그룹에 속해있지 않습니다.');
+    }
   }
 }
