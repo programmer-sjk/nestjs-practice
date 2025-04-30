fba7483a063e7bca0aff45ef21e3576da057352a
sjk5766 <wjdrnr6662@gmail.com>
2025-04-30 09:08:46 +0900

    feat: app 모듈에 mongodb 설정 추가

--- chatting/src/app.module.ts
+++ chatting/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
-import { UserModule } from './user/user.module';
-import { GroupModule } from './group/group.module';
 import { ChatModule } from './chat/chat.module';
+import { GroupModule } from './group/group.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule, GroupModule, ChatModule],
+  imports: [
+    TypeOrmModule.forRoot(connectionOptions),
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    UserModule,
+    GroupModule,
+    ChatModule,
+  ],
 })
 export class AppModule {}
