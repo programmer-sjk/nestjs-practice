3f1fe322dbaedef2405d584810a1d05478509d43
sjk5766 <wjdrnr6662@gmail.com>
2025-04-30 09:09:32 +0900

    feat: chat 모듈에 chat schema 설정 추가

--- chatting/src/chat/chat.module.ts
+++ chatting/src/chat/chat.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
-import { ChatService } from './chat.service';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ChatController } from './chat.controller';
+import { ChatService } from './chat.service';
+import { Chat, ChatSchema } from './schemas/chat.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+  ],
   providers: [ChatService],
-  controllers: [ChatController]
+  controllers: [ChatController],
 })
 export class ChatModule {}
