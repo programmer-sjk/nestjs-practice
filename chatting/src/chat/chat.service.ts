970ba2b02c34c8eb1a6e69d9b4a5d65be46a5d98
sjk5766 <wjdrnr6662@gmail.com>
2025-05-05 12:38:22 +0900

    feat: 그룹에 없는 사용자가 그룹에 채팅할 수 없는 검증 추가

--- chatting/src/chat/chat.service.ts
+++ chatting/src/chat/chat.service.ts
@@ -1,16 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { GroupService } from '../group/group.service';
 import { AddChatRequest } from './dto/add-chat.request';
 import { Chat } from './schemas/chat.schema';
 
 @Injectable()
 export class ChatService {
   constructor(
+    private readonly groupService: GroupService,
     @InjectModel(Chat.name) private readonly chatModel: Model<Chat>,
   ) {}
 
   async add(dto: AddChatRequest) {
+    await this.groupService.validateUserInGroup(dto.groupId, dto.ownerId);
     const data = new this.chatModel(dto);
     return data.save();
   }
