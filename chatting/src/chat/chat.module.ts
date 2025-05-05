924572efa1d15e6069370a90c43e4db634ca76f0
sjk5766 <wjdrnr6662@gmail.com>
2025-05-05 12:33:47 +0900

    feat: chat 모듈에 group 모듈 import

--- chatting/src/chat/chat.module.ts
+++ chatting/src/chat/chat.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { GroupModule } from '../group/group.module';
 import { ChatController } from './chat.controller';
 import { ChatService } from './chat.service';
 import { Chat, ChatSchema } from './schemas/chat.schema';
@@ -7,6 +8,7 @@ import { Chat, ChatSchema } from './schemas/chat.schema';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Chat.name, schema: ChatSchema }]),
+    GroupModule,
   ],
   providers: [ChatService],
   controllers: [ChatController],
