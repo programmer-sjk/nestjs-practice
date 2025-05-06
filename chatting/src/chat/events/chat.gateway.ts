7b6712fe3a00156d4e7ffa5291fbb3ff484eb389
sjk5766 <wjdrnr6662@gmail.com>
2025-05-06 22:25:18 +0900

    feat: chat 이벤트 받는 gateway 로직 추가

---
+++ chatting/src/chat/events/chat.gateway.ts
@@ -0,0 +1,17 @@
+import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+
+@WebSocketGateway()
+export class EventsGateway {
+  @WebSocketServer()
+  server: Server;
+
+  @SubscribeMessage('events')
+  findAll(@MessageBody() data: any): Observable<WsResponse<number>> {
+    return from([1, 2, 3]).pipe(map(item => ({ event: 'events', data: item })));
+  }
+
+  @SubscribeMessage('identity')
+  async identity(@MessageBody() data: number): Promise<number> {
+    return data;
+  }
+}
