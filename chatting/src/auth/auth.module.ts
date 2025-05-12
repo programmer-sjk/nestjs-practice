8303687e82f257f4d768950f8a93271d5603d343
sjk5766 <wjdrnr6662@gmail.com>
2025-05-12 12:14:09 +0900

    feat: auth 모듈에 jwt 모듈 imports

--- chatting/src/auth/auth.module.ts
+++ chatting/src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule],
   providers: [AuthService],
-  controllers: [AuthController]
+  controllers: [AuthController],
 })
 export class AuthModule {}
