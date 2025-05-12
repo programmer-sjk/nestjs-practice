599c763eb7fb05023c5fbae2f1556c92a7e622e4
sjk5766 <wjdrnr6662@gmail.com>
2025-05-12 12:57:45 +0900

    feat: app 모듈에 AuthGuard 전역 설정

--- chatting/src/app.module.ts
+++ chatting/src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
+import { AuthModule } from './auth/auth.module';
+import { AuthGuard } from './auth/guards/auth.guard';
 import { ChatModule } from './chat/chat.module';
 import { GroupModule } from './group/group.module';
 import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -17,6 +20,13 @@ import { AuthModule } from './auth/auth.module';
     GroupModule,
     ChatModule,
     AuthModule,
+    JwtModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
   ],
 })
 export class AppModule {}
