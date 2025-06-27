2770617746077715bef04f900b4f8cee5ed4d2a9
programmer-sjk <wjdrnr6662@gmail.com>
2025-06-27 18:18:24 +0900

    chore: ormconfig 파일 추가

---
+++ nestjs-aws/ormconfig.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
+
+export const connectionOptions: DataSourceOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'password',
+  database: 'aws',
+  logging: true,
+  synchronize: false,
+  entities: [__dirname + '/src/**/entities/*.{js,ts}'],
+  migrations: [__dirname + '/src/migrations/*.{js,ts}'],
+  migrationsTableName: 'migrations',
+  namingStrategy: new SnakeNamingStrategy(),
+};
+
+const dataSource = new DataSource(connectionOptions);
+export default dataSource;
