958c1c755d9786e886aea450a5aba9f71f4f331c
programmer-sjk <wjdrnr6662@gmail.com>
2025-06-28 17:29:00 +0900

    refactor: 마이그레이션 파일에 lint 적용

--- nestjs-aws/src/migrations/1751099219596-user.ts
+++ nestjs-aws/src/migrations/1751099219596-user.ts
@@ -1,15 +1,24 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class User1751099219596 implements MigrationInterface {
-    name = 'User1751099219596'
+  name = 'User1751099219596';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE \`user\` (\`id\` int NOT NULL AUTO_INCREMENT, \`name\` varchar(16) NOT NULL, \`email\` varchar(32) NOT NULL, \`password\` varchar(255) NOT NULL, \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`deleted_at\` datetime(6) NULL, INDEX \`email\` (\`email\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`user\` (
+        \`id\` int NOT NULL AUTO_INCREMENT, 
+        \`name\` varchar(16) NOT NULL, 
+        \`email\` varchar(32) NOT NULL, 
+        \`password\` varchar(255) NOT NULL, 
+        \`created_at\` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), 
+        \`deleted_at\` datetime(6) NULL, 
+        INDEX \`email\` (\`email\`), 
+        PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP INDEX \`email\` ON \`user\``);
-        await queryRunner.query(`DROP TABLE \`user\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP INDEX \`email\` ON \`user\``);
+    await queryRunner.query(`DROP TABLE \`user\``);
+  }
 }
