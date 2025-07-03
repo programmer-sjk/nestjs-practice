471b2ff8ae087664873d5e26683a4e7b8333f06b
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-03 18:51:57 +0900

    feat: user 서비스에서 userProfile 레포지터리 주입

--- nestjs-aws/src/users/user.module.ts
+++ nestjs-aws/src/users/user.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UserProfileRepository } from './repositories/user-profile.repository';
 import { UserRepository } from './repositories/user.repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, UserProfileRepository],
 })
 export class UsersModule {}
--- nestjs-aws/src/users/user.service.ts
+++ nestjs-aws/src/users/user.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { SignUpRequest } from './dto/sign-up.request';
+import { UserProfileRepository } from './repositories/user-profile.repository';
 import { UserRepository } from './repositories/user.repository';
 @Injectable()
 export class UserService {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly userProfileRepository: UserProfileRepository,
+  ) {}
 
   async signUp(dto: SignUpRequest) {
     return this.userRepository.save(dto.toEntity());
   }
+
+  async uploadProfile() {}
 }
