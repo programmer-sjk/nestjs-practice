e44f3ac2c8378d027eb72b764d74c99817d5564a
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-10 15:05:16 +0900

    feat: app 모듈에 config 모듈 global로 선언

--- nestjs-aws/src/app.module.ts
+++ nestjs-aws/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from 'ormconfig';
+import { UploadModule } from './upload/upload.module';
 import { UsersModule } from './users/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRoot(connectionOptions),
+    UsersModule,
+    UploadModule,
+  ],
 })
 export class AppModule {}
