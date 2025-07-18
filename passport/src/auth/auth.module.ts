2c3c0972f30d7a3993b1757efc27ded6a9139941
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-18 19:32:25 +0900

    feat: local 전략으로 인증기능

--- passport/src/auth/auth.module.ts
+++ passport/src/auth/auth.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../user/user.module';
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
-  providers: [AuthService]
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- passport/src/auth/auth.service.ts
+++ passport/src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '../user/entities/user.entity';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  validateUser(name: string, password: string) {
+    const user = this.userService.findOneByName(name);
+    if (user && user.password === password) {
+      return user;
+    }
+  }
+
+  login(user: User) {
+    const payload = { name: user.name, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
