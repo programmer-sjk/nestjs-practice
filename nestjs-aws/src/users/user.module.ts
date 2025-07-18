4dbbf72e8b4c7423e2e8b6ac9bbc9ce2e7ddd964
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-18 16:41:36 +0900

    feat: upload 기능을 upload service로 이관

--- nestjs-aws/src/users/user.module.ts
+++ nestjs-aws/src/users/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
+import { UploadModule } from '../upload/upload.module';
 import { UserProfileRepository } from './repositories/user-profile.repository';
 import { UserRepository } from './repositories/user.repository';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [UploadModule],
   controllers: [UserController],
   providers: [UserService, UserRepository, UserProfileRepository],
 })
--- nestjs-aws/src/users/user.service.ts
+++ nestjs-aws/src/users/user.service.ts
@@ -1,53 +1,24 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { UploadService } from '../upload/upload.service';
 import { SignUpRequest } from './dto/sign-up.request';
 import { UserProfileRepository } from './repositories/user-profile.repository';
 import { UserRepository } from './repositories/user.repository';
 
 @Injectable()
 export class UserService {
-  private readonly s3Client: S3Client;
-  private readonly bucketName: string;
-  private readonly region = 'us-east-1';
-  private readonly directory = 'local-upload-test';
-
   constructor(
-    private readonly configService: ConfigService,
+    private readonly uploadService: UploadService,
     private readonly userRepository: UserRepository,
     private readonly userProfileRepository: UserProfileRepository,
-  ) {
-    this.bucketName = this.configService.get('AWS_BUCKET_NAME') ?? '';
-    this.s3Client = new S3Client({
-      region: this.region,
-      credentials: {
-        accessKeyId: this.configService.get('AWS_ACCESS_KEY') ?? '',
-        secretAccessKey: this.configService.get('AWS_SECRET_KEY') ?? '',
-      },
-    });
-  }
+  ) {}
 
   async signUp(dto: SignUpRequest) {
     return this.userRepository.save(dto.toEntity());
   }
 
   async uploadProfile(userId: number, file: Express.Multer.File) {
-    const key = `${this.directory}/${Date.now()}-profile`;
-    const command = new PutObjectCommand({
-      Bucket: this.bucketName,
-      Key: key,
-      Body: file.buffer,
-      ContentType: file.mimetype,
-    });
-    try {
-      await this.s3Client.send(command);
-      const imagePath = `https://${this.bucketName}.s3.${this.region}.amazonaws.com/${key}`;
-
-      await this.userProfileRepository.save({ userId, path: imagePath });
-      return { imagePath };
-    } catch (error) {
-      console.error('S3 업로드 에러:', error);
-      throw new Error('파일 업로드에 실패했습니다.');
-    }
+    const imagePath = await this.uploadService.uploadProfile(file);
+    await this.userProfileRepository.save({ userId, path: imagePath });
+    return { imagePath };
   }
 }
