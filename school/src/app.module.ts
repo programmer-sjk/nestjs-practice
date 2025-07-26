de61e0c16952561b5230775521a9e86b07f07af3
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-26 14:50:31 +0900

    feat: app 모듈에 TypeOrmModule 추가

--- school/src/app.module.ts
+++ school/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { connectionOptions } from '../ormconfig';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), AuthModule, UserModule],
 })
 export class AppModule {}
