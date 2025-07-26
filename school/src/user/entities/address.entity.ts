603491b8070ea61fd08a416bdd18a3bf6b8c2609
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-26 14:50:40 +0900

    feat: 기존 엔티티에 @Entity 데코레이터 추가

--- school/src/user/entities/address.entity.ts
+++ school/src/user/entities/address.entity.ts
@@ -1,9 +1,12 @@
-export class Address {
-  id: number;
+import { Column } from 'typeorm';
 
+export class Address {
+  @Column()
   street: string;
 
+  @Column()
   city: string;
 
+  @Column()
   zipCode: string;
 }
--- school/src/user/entities/user.entity.ts
+++ school/src/user/entities/user.entity.ts
@@ -1,15 +1,34 @@
-import { Address } from 'cluster';
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  Index,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Address } from './address.entity';
 
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
 
+  @Index({ unique: true })
+  @Column()
+  email: string;
+
+  @Column()
   name: string;
 
+  @Column()
   password: string;
 
+  @Column(() => Address)
   address: Address;
 
+  @CreateDateColumn()
   createdAt: Date;
 
+  @DeleteDateColumn()
   deletedAt: Date;
 }
