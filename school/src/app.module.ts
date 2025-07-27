2be6e8bb81a240a35f8dbcf4bbfe6e7b956d11e1
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-27 15:11:09 +0900

    feat: course 모듈 추가

--- school/src/app.module.ts
+++ school/src/app.module.ts
@@ -3,8 +3,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { connectionOptions } from '../ormconfig';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), AuthModule, UserModule],
+  imports: [TypeOrmModule.forRoot(connectionOptions), AuthModule, UserModule, CourseModule],
 })
 export class AppModule {}
