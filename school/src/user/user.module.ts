ee7d768431da725018746c965d4b19608c6a5520
programmer-sjk <wjdrnr6662@gmail.com>
2025-07-27 15:30:44 +0900

    feat: user 레포지터리 추가

--- school/src/user/user.module.ts
+++ school/src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
+import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, UserRepository],
   exports: [UserService],
   controllers: [UserController],
 })
