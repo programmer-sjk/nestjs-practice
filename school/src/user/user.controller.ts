3baa60a996b6b65fc42a2f372f36f6a1fc6db309
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-01 19:22:07 +0900

    remove: 불필요한 API 제거

--- school/src/user/user.controller.ts
+++ school/src/user/user.controller.ts
@@ -1,5 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ResponseEntity } from '../common/response-entity';
 import { SignUpRequest } from './dtos/sign-up.request';
 import { UserService } from './user.service';
@@ -7,12 +6,6 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
-
-  @UseGuards(JwtAuthGuard)
-  @Get('/need-auth-api')
-  protectedApi() {
-    return 'jwt auth success';
-  }
 
   @Post()
   async signUp(@Body() request: SignUpRequest) {
