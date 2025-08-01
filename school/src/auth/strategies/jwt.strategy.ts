3ad0fdc951f55aea78f28a24c31123c3440c1aa5
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-01 19:21:52 +0900

    feat: jwt 인증 성공시 body에 userId 추가

--- school/src/auth/strategies/jwt.strategy.ts
+++ school/src/auth/strategies/jwt.strategy.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
+interface JwtPayload {
+  sub: number;
+}
+
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
@@ -12,7 +16,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: any) {
-    return { userId: payload.sub, username: payload.name };
+  validate(payload: JwtPayload) {
+    return { userId: payload.sub };
   }
 }
