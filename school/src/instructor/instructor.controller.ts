2bebc5cf03aef6479e4b55f768d15898d4cf1093
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-04 19:37:07 +0900

    feat: 강사 회원가입 기능

--- school/src/instructor/instructor.controller.ts
+++ school/src/instructor/instructor.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { InstructorSignUpRequest } from './dtos/instructor-sign-up.request';
+import { InstructorService } from './instructor.service';
 
 @Controller('instructor')
-export class InstructorController {}
+export class InstructorController {
+  constructor(private readonly instructorService: InstructorService) {}
+
+  @Post()
+  async signUp(@Body() body: InstructorSignUpRequest) {
+    return this.instructorService.signUp(body);
+  }
+}
--- school/src/instructor/instructor.service.ts
+++ school/src/instructor/instructor.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InstructorSignUpRequest } from './dtos/instructor-sign-up.request';
+import { InstructorRepository } from './instructor.repository';
 
 @Injectable()
-export class InstructorService {}
+export class InstructorService {
+  constructor(private readonly instructorRepository: InstructorRepository) {}
+
+  async signUp(dto: InstructorSignUpRequest) {
+    return this.instructorRepository.save(dto.toEntity());
+  }
+}
