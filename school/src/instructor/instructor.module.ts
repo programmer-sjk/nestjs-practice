e333a5b0abeec0d7d28e4f276b01b07fa73adf59
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-04 19:34:51 +0900

    feat: instructor 레포지터리 추가

--- school/src/instructor/instructor.module.ts
+++ school/src/instructor/instructor.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { InstructorService } from './instructor.service';
 import { InstructorController } from './instructor.controller';
+import { InstructorRepository } from './instructor.repository';
+import { InstructorService } from './instructor.service';
 
 @Module({
-  providers: [InstructorService],
-  controllers: [InstructorController]
+  providers: [InstructorService, InstructorRepository],
+  controllers: [InstructorController],
 })
 export class InstructorModule {}
