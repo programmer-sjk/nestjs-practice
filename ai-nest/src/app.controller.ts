66fcf392a8bba74e591dc03aab2040d772cdbcd3
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-08 15:32:18 +0900

    feat: ollama 모델을 호출하는 API 추가

--- ai-nest/src/app.controller.ts
+++ ai-nest/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import ollama from 'ollama';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  async getOllama(@Body('prompt') prompt: string) {
+    const response = await ollama.chat({
+      model: 'llama3.2:1b',
+      messages: [{ role: 'user', content: prompt }],
+    });
+
+    return response.message.content;
   }
 }
