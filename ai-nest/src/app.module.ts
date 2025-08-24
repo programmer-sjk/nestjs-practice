180bdf14e15b0d6a5368567cad6e2f268085cc6d
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-24 10:50:26 +0900

    feat: prompt 모듈, 컨트롤러, 서비스 추가

--- ai-nest/src/app.module.ts
+++ ai-nest/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PromptModule } from './prompt/prompt.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PromptModule],
   controllers: [AppController],
   providers: [AppService],
 })
