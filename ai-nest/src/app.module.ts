08b2a56ef58bc557b66e8dff4e4229c550259811
programmer-sjk <wjdrnr6662@gmail.com>
2025-08-24 10:51:12 +0900

    feat: lang-chain 모듈, 컨트롤러, 서비스 추가

--- ai-nest/src/app.module.ts
+++ ai-nest/src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PromptModule } from './prompt/prompt.module';
+import { LangchainModule } from './langchain/langchain.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), PromptModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), PromptModule, LangchainModule],
   controllers: [AppController],
   providers: [AppService],
 })
