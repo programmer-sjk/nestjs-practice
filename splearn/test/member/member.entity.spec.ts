9f6639754e254405df23c5e3743ee09598c060f0
programmer-sjk <wjdrnr6662@gmail.com>
2025-10-15 19:23:13 +0900

    test: member 엔티티 테스트 추가

--- splearn/test/member/member.entity.spec.ts
+++ splearn/test/member/member.entity.spec.ts
@@ -1,15 +1,64 @@
+import { BadRequestException } from '@nestjs/common';
+import { PasswordEncoderDomainService } from '../../src/member/domain-services/password-encoder.domain-service';
 import { Member } from '../../src/member/entities/member.entity';
 import { MemberStatus } from '../../src/member/enums/member-status.enum';
 
 describe('Member', () => {
-  it('Member 생성', () => {
+  let member: Member;
+
+  beforeEach(() => {
+    member = Member.create(
+      'test@example.com',
+      'test',
+      'password',
+      new PasswordEncoderDomainService(),
+    );
+  });
+
+  it('Member는 active 상태로 변경될 수 있다.', () => {
     // when
-    const member = new Member('test@example.com', 'test', 'password');
+    member.activate();
 
     // then
-    expect(member.email).toBe('test@example.com');
-    expect(member.nickname).toBe('test');
-    expect(member.passwordHash).toBe('password');
-    expect(member.status).toBe(MemberStatus.PENDING);
+    expect(member.status).toBe(MemberStatus.ACTIVE);
+  });
+
+  it('active된 상태의 member는 더 이상 active 상태로 변경될 수 없다.', () => {
+    // given
+    member.activate();
+
+    // when & then
+    expect(() => member.activate()).toThrow(
+      new BadRequestException('pending 상태가 아닙니다.'),
+    );
+  });
+
+  it('회원은 탈퇴할 수 있다.', () => {
+    // given
+    member.activate();
+
+    // when
+    member.deactivate();
+
+    // then
+    expect(member.status).toBe(MemberStatus.DEACTIVATED);
+  });
+
+  it('active된 회원만 탈퇴할 수 있다.', () => {
+    // when & then
+    expect(() => member.deactivate()).toThrow(
+      new BadRequestException('active 상태가 아닙니다.'),
+    );
+  });
+
+  it('회원은 비밀번호를 검증할 수 있다.', () => {
+    // when
+    const result = member.verifyPassword(
+      'password',
+      new PasswordEncoderDomainService(),
+    );
+
+    // then
+    expect(result).toBe(true);
   });
 });
