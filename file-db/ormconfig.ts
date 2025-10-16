5be6af2dbe5e504df71aa460560d4c86d6bbd6da
programmer-sjk <wjdrnr6662@gmail.com>
2025-10-16 13:32:53 +0900

    chore: database 경로 수정하고 로깅 옵션 추가

--- file-db/ormconfig.ts
+++ file-db/ormconfig.ts
@@ -2,9 +2,12 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 
 export const connectionOptions: DataSourceOptions = {
   type: 'sqlite',
-  database: 'database.sqlite',
+  database: './databases/database.sqlite',
   synchronize: false,
-  entities: [__dirname + '/src/**/entities/*.{js,ts}'],
+  entities: [__dirname + '/**/entities/*.js'],
+  migrations: [__dirname + '/migrations/*.js'],
+  migrationsTableName: 'migrations',
+  logging: true,
 };
 
 const dataSource = new DataSource(connectionOptions);
