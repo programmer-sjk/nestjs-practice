8ad7513609db259a1a5065e127046093e2ab3457
programmer-sjk <wjdrnr6662@gmail.com>
2025-10-25 11:23:07 +0900

    feat: 회원가입 메서드

--- splearn/src/member/member.service.ts
+++ splearn/src/member/member.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PasswordEncoderDomainService } from './domain-services/password-encoder.domain-service';
+import { Member } from './entities/member.entity';
 
 @Injectable()
-export class MemberService {}
+export class MemberService {
+  constructor(
+    private readonly passwordEncoderDomainService: PasswordEncoderDomainService,
+  ) {}
+
+  signUp(email: string, nickname: string, password: string) {
+    return Member.create(
+      email,
+      nickname,
+      password,
+      this.passwordEncoderDomainService,
+    );
+  }
+}
