6f08603c67195995a53c0ca7221131c751264dcf
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-01 14:52:50 +0900

    feat: MemberRegisterRequest를 interface -> class로 수정

--- splearn/src/member/domain/member-register.request.ts
+++ splearn/src/member/domain/member-register.request.ts
@@ -1,5 +1,16 @@
-export interface MemberRegisterRequest {
+import { IsEmail, IsNotEmpty, IsString, MaxLength } from 'class-validator';
+
+export class MemberRegisterRequest {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @MaxLength(16)
+  @IsString()
   nickname: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
