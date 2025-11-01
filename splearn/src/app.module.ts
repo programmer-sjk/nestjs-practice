911bba97c3f84c62e3dd73d75cbf15693c214bf0
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-01 15:08:59 +0900

    chore: typeorm 설정 추가

--- splearn/src/app.module.ts
+++ splearn/src/app.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Member } from './member/domain/member.entity';
 import { MemberModule } from './member/member.module';
 
 @Module({
-  imports: [MemberModule]
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: ':memory:',
+      entities: [Member],
+    }),
+    MemberModule,
+  ],
 })
 export class AppModule {}
