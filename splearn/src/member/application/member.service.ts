504f975163826a2a619e3d303c597113dbcbe3c8
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-03 20:46:26 +0900

    feat: activate 메서드 구현

--- splearn/src/member/application/member.service.ts
+++ splearn/src/member/application/member.service.ts
@@ -1,16 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PasswordEncoderDomainService } from '../domain-services/password-encoder.domain-service';
 import { MemberRegisterRequest } from '../domain/member-register.request';
 import { Member } from '../domain/member.entity';
 import { MemberRegister } from './provided/member-register.interface';
+import { MemberRepository } from './required/member.repository';
 
 @Injectable()
 export class MemberService implements MemberRegister {
   constructor(
     private readonly passwordEncoderDomainService: PasswordEncoderDomainService,
+    private readonly memberRepository: MemberRepository,
   ) {}
 
-  register(dto: MemberRegisterRequest) {
-    return Member.create(dto, this.passwordEncoderDomainService);
+  async register(dto: MemberRegisterRequest) {
+    const member = Member.create(dto, this.passwordEncoderDomainService);
+    return await this.memberRepository.save(member);
+  }
+
+  async activate(id: number) {
+    const member = await this.memberRepository.findOneBy({ id });
+    if (!member) {
+      throw new NotFoundException('Member not found');
+    }
+
+    member.activate();
+    return await this.memberRepository.save(member);
   }
 }
--- splearn/src/member/member.module.ts
+++ splearn/src/member/member.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MemberService } from './application/member.service';
+import { MemberRepository } from './application/required/member.repository';
+import { PasswordEncoderDomainService } from './domain-services/password-encoder.domain-service';
 
 @Module({
-  providers: [MemberService],
+  providers: [MemberService, PasswordEncoderDomainService, MemberRepository],
 })
 export class MemberModule {}
