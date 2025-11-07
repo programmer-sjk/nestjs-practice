e1ff5250b54cc2c45a428c653bd0f006ff7fef55
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-07 17:48:43 +0900

    chore: lint 에러 수정

--- splearn/src/common/transformers/value-object.transformer.ts
+++ splearn/src/common/transformers/value-object.transformer.ts
@@ -1,14 +1,14 @@
 import { ValueTransformer } from 'typeorm';
 
-/**
- * Value Object용 Transformer 생성 함수
- * Value Object는 반드시 'value' getter를 가지고 있어야 합니다.
- */
-export function valueObjectTransformer<T>(
+interface ValueObject {
+  value: string;
+}
+
+export function valueObjectTransformer<T extends ValueObject>(
   ValueObjectClass: new (value: string) => T,
 ): ValueTransformer {
   return {
-    to: (valueObject: any) => valueObject?.value ?? null,
+    to: (valueObject?: T | null) => valueObject?.value ?? null,
     from: (dbValue: string) => (dbValue ? new ValueObjectClass(dbValue) : null),
   };
 }
