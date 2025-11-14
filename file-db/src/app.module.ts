39b612bcaba5ad2e19cd8f6572d9f1817919fe23
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-14 17:23:20 +0900

    remove: file db 프로젝트 제거

--- file-db/src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { connectionOptions } from '../ormconfig';
-import { UserModule } from './user/user.module';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(connectionOptions), UserModule],
-})
-export class AppModule {}
