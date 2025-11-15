a10e36f785e623046760f7cecdfa9af3b1295e5c
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-15 12:13:34 +0900

    feat: 상품 단일, 목록 조회 API 추가

--- projects/product/src/product/application/product.service.ts
+++ projects/product/src/product/application/product.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { ProductRepository } from '../infrastructure/persistence/product.repository';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async find(id: number) {
+    return this.productRepository.findOneById(id);
+  }
+
+  async findAll() {
+    return this.productRepository.findAll();
+  }
+}
--- projects/product/src/product/presentation/product.controller.ts
+++ projects/product/src/product/presentation/product.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ProductService } from '../application/product.service';
 
-@Controller('product')
-export class ProductController {}
+@Controller('products')
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get(':id')
+  async find(@Param('id') id: number) {
+    return this.productService.find(id);
+  }
+
+  @Get()
+  async findAll() {
+    return this.productService.findAll();
+  }
+}
