97ad7babb9e0c50a4965ff38c1b80c368cdd23e6
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-15 12:22:20 +0900

    feat: 공통 응답용 ResponseEntity 클래스 추가

---
+++ projects/product/src/common/response-entity.ts
@@ -0,0 +1,43 @@
+import { Exclude, Expose } from 'class-transformer';
+
+export class ResponseEntity<T> {
+  private static readonly SUCCESS = true;
+  private static readonly FAIL = false;
+
+  @Exclude() readonly _success: boolean;
+  @Exclude() readonly _message: string;
+  @Exclude() readonly _data: T;
+
+  private constructor(success: boolean, message: string, data: T) {
+    this._success = success;
+    this._message = message;
+    this._data = data;
+  }
+
+  static OK<T>(data?: T) {
+    return new ResponseEntity(ResponseEntity.SUCCESS, '', data ?? '');
+  }
+
+  static ERROR(message: string) {
+    return new ResponseEntity(ResponseEntity.FAIL, message, '');
+  }
+
+  static ERROR_WITH<T>(message: string, data: T) {
+    return new ResponseEntity(ResponseEntity.FAIL, message, data);
+  }
+
+  @Expose()
+  get success(): boolean {
+    return this._success;
+  }
+
+  @Expose()
+  get message(): string {
+    return this._message;
+  }
+
+  @Expose()
+  get data(): T {
+    return this._data;
+  }
+}
