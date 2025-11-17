678c276eef0917034792737dc52d950e0b95640f
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-17 10:23:31 +0900

    chore: test db를 생성하는 sql 추가

---
+++ projects/product/docker/mysql/init/01-create-test-database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+-- 필요시 별도의 테스트 사용자 생성 (선택사항)
+-- CREATE USER IF NOT EXISTS 'test_user'@'%' IDENTIFIED BY 'test_password';
+-- GRANT ALL PRIVILEGES ON commerce_test.* TO 'test_user'@'%';
+-- FLUSH PRIVILEGES;
+
