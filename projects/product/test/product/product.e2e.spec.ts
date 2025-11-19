6e339969b93ca86ff1fcf691c554592e7b931144
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-19 20:08:50 +0900

    test: e2e 테스트 describe 추가

--- projects/product/test/product/product.e2e.spec.ts
+++ projects/product/test/product/product.e2e.spec.ts
@@ -36,77 +36,92 @@ describe('Product E2E', () => {
     await app.close();
   });
 
-  it('특정 상품을 조회할 수 있다.', async () => {
-    const storeId = 1;
-    const basePrice = 10000;
-    const productName = '2025년 신상 패딩';
+  describe('GET /v1/products/:id', () => {
+    it('특정 상품을 조회할 수 있다.', async () => {
+      // given
+      const storeId = 1;
+      const basePrice = 10000;
+      const productName = '2025년 신상 패딩';
 
-    const product = await productRepository.save(
-      ProductFactory.create(storeId, productName, basePrice),
-    );
+      const product = await productRepository.save(
+        ProductFactory.create(storeId, productName, basePrice),
+      );
 
-    const response = await request(app.getHttpServer())
-      .get(`/v1/products/${product.id}`)
-      .expect(HttpStatus.OK);
+      // when
+      const response = await request(app.getHttpServer())
+        .get(`/v1/products/${product.id}`)
+        .expect(HttpStatus.OK);
 
-    const result = response.body as ResponseEntity<Product>;
-    expect(result.data.id).toBe(product.id);
-    expect(result.data.name).toBe('2025년 신상 패딩');
-    expect(result.data.basePrice).toBe(10000);
+      // then
+      const result = response.body as ResponseEntity<Product>;
+      expect(result.data.id).toBe(product.id);
+      expect(result.data.name).toBe('2025년 신상 패딩');
+      expect(result.data.basePrice).toBe(10000);
+    });
   });
 
-  it('상품 목록을 조회할 수 있다.', async () => {
-    const storeId = 1;
-    const basePrice = 10000;
-    const productName = '2025년 신상 패딩';
+  describe('GET /v1/products', () => {
+    it('상품 목록을 조회할 수 있다.', async () => {
+      // given
+      const storeId = 1;
+      const basePrice = 10000;
+      const productName = '2025년 신상 패딩';
 
-    const product = await productRepository.save(
-      ProductFactory.create(storeId, productName, basePrice),
-    );
+      const product = await productRepository.save(
+        ProductFactory.create(storeId, productName, basePrice),
+      );
 
-    const productName2 = '2025년 신상 셔츠';
-    const basePrice2 = 20000;
-    const product2 = await productRepository.save(
-      ProductFactory.create(storeId, productName2, basePrice2),
-    );
+      const productName2 = '2025년 신상 셔츠';
+      const basePrice2 = 20000;
+      const product2 = await productRepository.save(
+        ProductFactory.create(storeId, productName2, basePrice2),
+      );
 
-    const response = await request(app.getHttpServer())
-      .get(`/v1/products`)
-      .expect(HttpStatus.OK);
+      // when
+      const response = await request(app.getHttpServer())
+        .get(`/v1/products`)
+        .expect(HttpStatus.OK);
 
-    const result = response.body as ResponseEntity<Product[]>;
-    expect(result.data.length).toBe(2);
+      // then
+      const result = response.body as ResponseEntity<Product[]>;
+      expect(result.data.length).toBe(2);
 
-    expect(result.data[0].id).toBe(product.id);
-    expect(result.data[0].name).toBe('2025년 신상 패딩');
-    expect(result.data[0].basePrice).toBe(10000);
+      expect(result.data[0].id).toBe(product.id);
+      expect(result.data[0].name).toBe('2025년 신상 패딩');
+      expect(result.data[0].basePrice).toBe(10000);
 
-    expect(result.data[1].id).toBe(product2.id);
-    expect(result.data[1].name).toBe('2025년 신상 셔츠');
-    expect(result.data[1].basePrice).toBe(20000);
+      expect(result.data[1].id).toBe(product2.id);
+      expect(result.data[1].name).toBe('2025년 신상 셔츠');
+      expect(result.data[1].basePrice).toBe(20000);
+    });
   });
 
-  it('상품을 등록할 수 있다.', async () => {
-    const storeId = 1;
-    const productName = '2025년 신상 패딩';
-    const basePrice = 10000;
+  describe('POST /v1/products', () => {
+    it('상품을 등록할 수 있다.', async () => {
+      // given
+      const storeId = 1;
+      const productName = '2025년 신상 패딩';
+      const basePrice = 10000;
 
-    const requestDto = ProductRegisterRequestFactory.create(
-      storeId,
-      productName,
-      basePrice,
-    );
+      const requestDto = ProductRegisterRequestFactory.create(
+        storeId,
+        productName,
+        basePrice,
+      );
 
-    await request(app.getHttpServer())
-      .post(`/v1/products`)
-      .send(requestDto)
-      .expect(HttpStatus.CREATED);
+      // when
+      await request(app.getHttpServer())
+        .post(`/v1/products`)
+        .send(requestDto)
+        .expect(HttpStatus.CREATED);
 
-    const product = await productRepository.find();
-    expect(product.length).toBe(1);
-    expect(product[0].storeId).toBe(storeId);
-    expect(product[0].name).toBe(productName);
-    expect(product[0].basePrice).toBe(basePrice);
-    expect(product[0].status).toBe(ProductStatus.DRAFT);
+      // then
+      const product = await productRepository.find();
+      expect(product.length).toBe(1);
+      expect(product[0].storeId).toBe(storeId);
+      expect(product[0].name).toBe(productName);
+      expect(product[0].basePrice).toBe(basePrice);
+      expect(product[0].status).toBe(ProductStatus.DRAFT);
+    });
   });
 });
