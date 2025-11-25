bf2f9482fa14911501235a7fd76fd33c7165594b
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-25 10:12:34 +0900

    feat: nest app에 swagger 설정 추가

--- projects/product/src/common/set-nest-app.ts
+++ projects/product/src/common/set-nest-app.ts
@@ -4,9 +4,22 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export function setNestApp(app: INestApplication) {
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('v1');
+
+  setupSwagger(app);
+}
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Api Document')
+    .setVersion('1.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 }
