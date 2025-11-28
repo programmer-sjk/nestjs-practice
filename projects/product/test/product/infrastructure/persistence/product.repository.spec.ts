577d9b30469b1d23c47003e7e3a875ad282762e0
programmer-sjk <wjdrnr6662@gmail.com>
2025-11-28 13:13:52 +0900

    test: product repository에 대한 테스트 코드 추가

--- projects/product/test/product/infrastructure/persistence/product.repository.spec.ts
+++ projects/product/test/product/infrastructure/persistence/product.repository.spec.ts
@@ -1,27 +1,26 @@
-import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProductUpdateRequest } from '../../../../src/product/application/dto/product-update.request';
-import { ProductService } from '../../../../src/product/application/services/product.service';
-import { ProductStatus } from '../../../../src/product/domain/enums/product-status.enum';
+import { DataSource, Repository } from 'typeorm';
+import { Product } from '../../../../src/product/domain/entities/product.entity';
 import { ProductRepository } from '../../../../src/product/infrastructure/persistence/product.repository';
 import { ProductModule } from '../../../../src/product/product.module';
-import { ProductRegisterRequestFactory } from '../../../fixtures/product-register-request.factory';
 import { ProductFactory } from '../../../fixtures/product.factory';
 import { testConnectionOptions } from '../../../test-ormconfig';
 
-describe('ProductService', () => {
+describe('ProductRepository', () => {
   let module: TestingModule;
-  let service: ProductService;
-  let productRepository: ProductRepository;
+  let repository: ProductRepository;
+  let productRepository: Repository<Product>;
+  let dataSource: DataSource;
 
   beforeAll(async () => {
     module = await Test.createTestingModule({
       imports: [TypeOrmModule.forRoot(testConnectionOptions), ProductModule],
     }).compile();
+    repository = module.get<ProductRepository>('IProductRepository');
 
-    service = module.get<ProductService>(ProductService);
-    productRepository = module.get<ProductRepository>(ProductRepository);
+    dataSource = module.get<DataSource>(DataSource);
+    productRepository = dataSource.getRepository(Product);
   });
 
   beforeEach(async () => {
@@ -32,35 +31,24 @@ describe('ProductService', () => {
     await module.close();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-
-  describe('find', () => {
-    it('특정 상품을 조회할 수 있다.', async () => {
+  describe('findOneById', () => {
+    it('id로 상품을 조회할 수 있다.', async () => {
       // given
       const storeId = 1;
       const basePrice = 10000;
       const productName = '2025년 신상 패딩';
 
-      const product = await productRepository.save(
+      const product = await repository.save(
         ProductFactory.create(storeId, productName, basePrice),
       );
 
       // when
-      const result = await service.find(product.id);
+      const result = await repository.findOneById(product.id);
 
       // then
-      expect(result.id).toBe(product.id);
-      expect(result.name).toBe(productName);
-      expect(result.basePrice).toBe(basePrice);
-    });
-
-    it('상품이 없다면 예외가 발생한다.', async () => {
-      // when
-      await expect(service.find(-1)).rejects.toThrow(
-        new NotFoundException('Product not found'),
-      );
+      expect(result?.id).toBe(product.id);
+      expect(result?.name).toBe(productName);
+      expect(result?.basePrice).toBe(basePrice);
     });
   });
 
@@ -71,12 +59,14 @@ describe('ProductService', () => {
       const basePrice = 10000;
       const productName = '2025년 신상 패딩';
 
-      const product = await productRepository.save(
+      const product = await repository.save(
         ProductFactory.create(storeId, productName, basePrice),
       );
 
       // when
-      const result = await service.findAll();
+      const result = await repository.findAll();
+
+      // then
       expect(result.length).toBe(1);
       expect(result[0].id).toBe(product.id);
       expect(result[0].name).toBe(productName);
@@ -84,21 +74,17 @@ describe('ProductService', () => {
     });
   });
 
-  describe('register', () => {
-    it('상품을 등록할 수 있다.', async () => {
+  describe('save', () => {
+    it('상품을 저장할 수 있다.', async () => {
       // given
       const storeId = 1;
       const productName = '2025년 신상 패딩';
       const basePrice = 10000;
 
-      const requestDto = ProductRegisterRequestFactory.create(
-        storeId,
-        productName,
-        basePrice,
-      );
-
       // when
-      await service.register(requestDto);
+      await repository.save(
+        ProductFactory.create(storeId, productName, basePrice),
+      );
 
       // then
       const product = await productRepository.find();
@@ -106,48 +92,22 @@ describe('ProductService', () => {
       expect(product[0].storeId).toBe(storeId);
       expect(product[0].name).toBe(productName);
       expect(product[0].basePrice).toBe(basePrice);
-      expect(product[0].status).toBe(ProductStatus.DRAFT);
-    });
-  });
-
-  describe('update', () => {
-    it('상품을 수정할 수 있다.', async () => {
-      // given
-      const storeId = 1;
-      const basePrice = 10000;
-      const productName = '2025년 신상 패딩';
-
-      const product = await productRepository.save(
-        ProductFactory.create(storeId, productName, basePrice),
-      );
-
-      const dto = new ProductUpdateRequest();
-      dto.name = '2025년 신상 패딩 2';
-      dto.basePrice = 15000;
-
-      // when
-      await service.update(product.id, dto);
-
-      // then
-      const result = await productRepository.findOneById(product.id);
-      expect(result?.name).toBe('2025년 신상 패딩 2');
-      expect(result?.basePrice).toBe(15000);
     });
   });
 
   describe('remove', () => {
-    it('상품을 삭제할 수 있다.', async () => {
+    it('id에 해당하는 상품을 삭제할 수 있다.', async () => {
       // given
       const storeId = 1;
       const basePrice = 10000;
       const productName = '2025년 신상 패딩';
 
-      const product = await productRepository.save(
+      const product = await repository.save(
         ProductFactory.create(storeId, productName, basePrice),
       );
 
       // when
-      await service.remove(product.id);
+      await repository.remove(product);
 
       // then
       const result = await productRepository.find();
