a0d2d56f91a2741346f6885edce1ea6cf229c6bd
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-03 15:51:06 +0900

    feat: 상점 관리자 회원가입 API에 swagger 적용

--- projects/product/src/merchant/presentation/merchant.controller.ts
+++ projects/product/src/merchant/presentation/merchant.controller.ts
@@ -1,12 +1,23 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { MerchantService } from '../application/services/merchant.service';
+import {
+  ApiExtraModels,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { ResponseEntity } from '../../common/response-entity';
+import { MutationResponseSchema } from '../../common/swagger/response-schema';
 import { MerchantSignUpRequest } from '../application/dto/merchant-signup.request';
+import { MerchantService } from '../application/services/merchant.service';
 
-@Controller('merchant')
+@ApiTags('merchants')
+@ApiExtraModels(ResponseEntity)
+@Controller('merchants')
 export class MerchantController {
   constructor(private readonly merchantService: MerchantService) {}
 
+  @ApiOperation({ summary: '상점 관리자 회원가입' })
+  @ApiResponse(MutationResponseSchema)
   @Post()
   async signUp(@Body() request: MerchantSignUpRequest) {
     await this.merchantService.signUp(request);
