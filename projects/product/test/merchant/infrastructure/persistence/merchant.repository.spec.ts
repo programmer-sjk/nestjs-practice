5a9fd802822be4f27a49f009bbf173fb35a83475
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-03 14:24:04 +0900

    test: merchant repository에 대한 테스트 코드 추가

---
+++ projects/product/test/merchant/infrastructure/persistence/merchant.repository.spec.ts
@@ -0,0 +1,48 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource, Repository } from 'typeorm';
+import { Merchant } from '../../../../src/merchant/domain/entities/merchant.entity';
+import { MerchantRepository } from '../../../../src/merchant/infrastructure/persistence/merchant.repository';
+import { MerchantModule } from '../../../../src/merchant/merchant.module';
+import { MerchantFactory } from '../../../fixtures/merchant.factory';
+import { testConnectionOptions } from '../../../test-ormconfig';
+
+describe('MerchantRepository', () => {
+  let module: TestingModule;
+  let repository: MerchantRepository;
+  let merchantRepository: Repository<Merchant>;
+  let dataSource: DataSource;
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      imports: [TypeOrmModule.forRoot(testConnectionOptions), MerchantModule],
+    }).compile();
+    repository = module.get<MerchantRepository>('IMerchantRepository');
+
+    dataSource = module.get<DataSource>(DataSource);
+    merchantRepository = dataSource.getRepository(Merchant);
+  });
+
+  beforeEach(async () => {
+    await merchantRepository.clear();
+  });
+
+  afterAll(async () => {
+    await module.close();
+  });
+
+  describe('save', () => {
+    it('상점 관리자를 저장할 수 있다.', async () => {
+      // given
+      const email = 'test@example.com';
+
+      // when
+      await repository.save(MerchantFactory.create(email));
+
+      // then
+      const merchant = await merchantRepository.find();
+      expect(merchant.length).toBe(1);
+      expect(merchant[0].email).toBe(email);
+    });
+  });
+});
