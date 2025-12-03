e682d5f2a49a4bad34427ea724f0ab6a7ae38c54
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-03 10:31:31 +0900

    feat: merchant 레포지터리 추가

---
+++ projects/product/src/merchant/infrastructure/persistence/merchant.repository.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Merchant } from '../../domain/entities/merchant.entity';
+import { IMerchantRepository } from '../../domain/repositories/merchant-repository.interface';
+
+@Injectable()
+export class MerchantRepository implements IMerchantRepository {
+  constructor(
+    @InjectRepository(Merchant)
+    private readonly repository: Repository<Merchant>,
+  ) {}
+
+  async findOneById(id: number) {
+    return await this.repository.findOneBy({ id });
+  }
+
+  async findAll() {
+    return await this.repository.find();
+  }
+
+  async save(merchant: Merchant) {
+    return await this.repository.save(merchant);
+  }
+
+  async remove(merchant: Merchant) {
+    return await this.repository.remove(merchant);
+  }
+}
