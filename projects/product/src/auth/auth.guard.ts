b51e16457f94f5f0ddbd9db0ce0bebf6fb9d8b3a
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-05 17:21:03 +0900

    feat: auth guard 추가

---
+++ projects/product/src/auth/auth.guard.ts
@@ -0,0 +1,67 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+import { ROLE_KEY } from './decorators/roles.decorator';
+import { Role } from './enums/role.enum';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  private readonly jwtSecret: string;
+
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly reflector: Reflector,
+  ) {
+    this.jwtSecret = this.configService.getOrThrow<string>(
+      'JWT_SECRET',
+    ) as string;
+  }
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const roles = this.reflector.getAllAndOverride<Role[]>(ROLE_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!roles) {
+      return true;
+    }
+
+    const request = context.switchToHttp().getRequest<Request>();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      return true;
+    }
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    let payload: JwtPayload;
+    try {
+      payload = await this.jwtService.verifyAsync(token, {
+        secret: this.jwtSecret,
+      });
+
+      request['merchantId'] = payload.merchantId;
+      return true;
+    } catch {
+      throw new UnauthorizedException();
+    }
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
