757aed477943176df7096ce886c84d3afd15268e
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-09 17:12:59 +0900

    feat: 상점 모듈 provider에 repository 추가

--- projects/product/src/store/store.module.ts
+++ projects/product/src/store/store.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoreService } from './application/services/store.service';
+import { Store } from './domain/entities/store.entity';
+import { StoreRepository } from './infrastructure/persistence/store.repository';
 import { StoreController } from './presentation/store.controller';
 
 @Module({
-  providers: [StoreService],
-  controllers: [StoreController]
+  imports: [TypeOrmModule.forFeature([Store])],
+  providers: [
+    StoreService,
+    {
+      provide: 'IStoreRepository',
+      useClass: StoreRepository,
+    },
+  ],
+  controllers: [StoreController],
 })
 export class StoreModule {}
