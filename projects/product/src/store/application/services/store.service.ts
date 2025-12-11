6eb65cb97cf90b5ca9fc8a8a4ca54dffde927392
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-11 20:31:18 +0900

    feat: 상점 등록 API 추가

--- projects/product/src/store/application/services/store.service.ts
+++ projects/product/src/store/application/services/store.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import * as storeRepositoryInterface from '../../domain/repositories/store-repository.interface';
+import { StoreRegisterRequest } from '../dto/store-register.request';
 
 @Injectable()
-export class StoreService {}
+export class StoreService {
+  constructor(
+    @Inject('IStoreRepository')
+    private readonly storeRepository: storeRepositoryInterface.IStoreRepository,
+  ) {}
+
+  async register(merchantId: number, dto: StoreRegisterRequest) {
+    await this.storeRepository.save(dto.toEntity(merchantId));
+  }
+}
--- projects/product/src/store/presentation/store.controller.ts
+++ projects/product/src/store/presentation/store.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Role } from '../../auth/domain/enums/role.enum';
+import { Roles } from '../../auth/infrastructure/decorators/roles.decorator';
+import { ResponseEntity } from '../../common/response-entity';
+import { MutationResponseSchema } from '../../common/swagger/response-schema';
+import { StoreRegisterRequest } from '../application/dto/store-register.request';
+import { StoreService } from '../application/services/store.service';
 
-@Controller('store')
-export class StoreController {}
+@Roles([Role.MERCHANT])
+@Controller('stores')
+export class StoreController {
+  constructor(private readonly storeService: StoreService) {}
+
+  @ApiOperation({ summary: '상점 등록' })
+  @ApiResponse(MutationResponseSchema)
+  @Post()
+  async register(@Body() request: StoreRegisterRequest, @Req() req: Request) {
+    await this.storeService.register(req['merchantId'], request);
+    return ResponseEntity.OK();
+  }
+}
