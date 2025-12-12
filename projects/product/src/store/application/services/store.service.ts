d44b7e4e76599d937cc73717283afa11e82fa93e
programmer-sjk <wjdrnr6662@gmail.com>
2025-12-12 17:37:16 +0900

    refactor: import 경로 잘못 된것 수정

--- projects/product/src/store/application/services/store.service.ts
+++ projects/product/src/store/application/services/store.service.ts
@@ -1,12 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
-import * as storeRepositoryInterface from '../../domain/repositories/store-repository.interface';
+import type { IStoreRepository } from '../../domain/repositories/store-repository.interface';
 import { StoreRegisterRequest } from '../dto/store-register.request';
 
 @Injectable()
 export class StoreService {
   constructor(
     @Inject('IStoreRepository')
-    private readonly storeRepository: storeRepositoryInterface.IStoreRepository,
+    private readonly storeRepository: IStoreRepository,
   ) {}
 
   async register(merchantId: number, dto: StoreRegisterRequest) {
